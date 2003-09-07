c505d9a0f7ba0683aa14d713d09503abc32ab070
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2003-09-07 17:27:50 +0000

    in the wrong place

--- xwords4/xwords4/dawg/swap.c
+++
@@ -1,9 +0,0 @@
-
-/*  #ifdef BIG_ENDIAN */
-/*  unsigned short swap_short(unsigned short s) { return s; } */
-/*  unsigned long swap_long(unsigned long l ) { return l; } */
-
-/*  #else */
-
-
-/*  #endif */
