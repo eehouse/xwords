69bca7d86d8cf180aacd9d427332f4c0b956736e
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2003-11-20 04:40:34 +0000

    checkin from personal archives

---
+++ wince/ceblank.h
@@ -0,0 +1,36 @@
+/* -*-mode: C; fill-column: 77; c-basic-offset: 4; -*- */
+/* 
+ * Copyright 2002 by Eric House (fixin@peak.org).  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef _CEBLANK_H_
+#define _CEBLANK_H_
+
+#include "stdafx.h" 
+#include "cemain.h"
+
+typedef struct BlankDialogState {
+    PickInfo* pi;
+    XP_U16 playerNum;
+    XP_UCHAR4* texts;
+    XP_U16 nTiles;
+    XP_S16 result;
+} BlankDialogState; 
+
+LRESULT CALLBACK BlankDlg(HWND, UINT, WPARAM, LPARAM);
+
+#endif
