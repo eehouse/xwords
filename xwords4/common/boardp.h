05f3232d0a0f745589ad42734cc553f4e0a2fd14
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2004-02-12 04:19:27 +0000

    export functions

--- xwords4/common/boardp.h
+++ xwords4/common/boardp.h
@@ -178,6 +178,8 @@ XP_Bool moveTileToArrowLoc( BoardCtxt* board, XP_U8 index );
 XP_U16 indexForBits( XP_U8 bits );
 XP_Bool rectContainsPt( XP_Rect* rect1, XP_U16 x, XP_U16 y );
 XP_Bool checkRevealTray( BoardCtxt* board );
+void invalTilesUnderRect( BoardCtxt* board, XP_Rect* rect );
+XP_Bool rectsIntersect( XP_Rect* rect1, XP_Rect* rect2 );
 
 #ifdef KEYBOARD_NAV
 XP_Bool tray_moveCursor( BoardCtxt* board, XP_Key cursorKey );
