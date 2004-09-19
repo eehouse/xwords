92a767328633cc311ff835274b9a5a7401399107
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2004-09-19 14:43:43 +0000

    fix order for 1-byte args

--- xwords4/palm/pace_man.h
+++ xwords4/palm/pace_man.h
@@ -6,16 +6,19 @@
 #ifdef XW_TARGET_PNO
 
 #include "pnostate.h"
+#include "xptypes.h"
 
 extern Int16 StrPrintF( Char* s, const Char* formatStr, ... );
 extern Int16 StrVPrintF( Char* s, const Char* formatStr, _Palm_va_list arg );
 extern void TimSecondsToDateTime( UInt32 seconds, DateTimeType* dateTimeP );
 
-
-/* #define FUNC_HEADER(n) printf( "%s called", #n ); */
-/* #define FUNC_TAIL(n)   printf( "%s done", #n ); */
-#define FUNC_HEADER(n)
-#define FUNC_TAIL(n)
+#if 0
+# define FUNC_HEADER(n) XP_LOGF( #n " called" )
+# define FUNC_TAIL(n)   XP_LOGF( #n " done" )
+#else
+# define FUNC_HEADER(n)
+# define FUNC_TAIL(n)
+#endif
 
 #define ADD_TO_STACK4(val,offset) \
     (unsigned char)(((unsigned long)val) >> 24), \
@@ -28,8 +31,8 @@ extern void TimSecondsToDateTime( UInt32 seconds, DateTimeType* dateTimeP );
     (unsigned char)(((unsigned short)val) >> 0)
 
 #define ADD_TO_STACK1(val,offset) \
-    (unsigned char)(0), \
-    (unsigned char)((val) >> 0)
+    (unsigned char)(val), \
+    (unsigned char)(0)
 
 PNOState* getStorageLoc();
 #define GET_CALLBACK_STATE() getStorageLoc()
