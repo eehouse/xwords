0f71b4fb615be2b938c33038e25d336d85e71c8e
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2004-09-21 04:49:27 +0000

    add event-related functions.  And general cleanup.

--- palm/pace_man.h
+++ palm/pace_man.h
@@ -1,9 +1,28 @@
 /* -*-mode: C; fill-column: 77; c-basic-offset: 4; -*- */
+/* 
+ * Copyright 2004 by Eric House (fixin@peak.org).  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
-#ifndef _ARMFUNC_HC_H_
-#define _ARMFUNC_HC_H_
+#ifndef _PACE_MAN_H_
+#define _PACE_MAN_H_
 
 #ifdef XW_TARGET_PNO
+
+#include <Form.h>
 
 #include "pnostate.h"
 #include "xptypes.h"
@@ -11,6 +30,15 @@
 extern Int16 StrPrintF( Char* s, const Char* formatStr, ... );
 extern Int16 StrVPrintF( Char* s, const Char* formatStr, _Palm_va_list arg );
 extern void TimSecondsToDateTime( UInt32 seconds, DateTimeType* dateTimeP );
+extern Boolean SysHandleEvent( EventPtr eventP );
+extern void EvtGetEvent( EventType* event, Int32 timeout );
+extern Boolean FrmDispatchEvent( EventType* eventP );
+extern Boolean MenuHandleEvent( MenuBarType* menuP, EventType* event, 
+                                UInt16* error );
+extern void FrmSetEventHandler( FormType* formP, 
+                                FormEventHandlerType* handler );
+extern void EvtAddEventToQueue( const EventType* event );
+
 
 #if 0
 # define FUNC_HEADER(n) XP_LOGF( #n " called" )
@@ -20,23 +48,19 @@ extern void TimSecondsToDateTime( UInt32 seconds, DateTimeType* dateTimeP );
 # define FUNC_TAIL(n)
 #endif
 
-#define ADD_TO_STACK4(val,offset) \
-    (unsigned char)(((unsigned long)val) >> 24), \
-    (unsigned char)(((unsigned long)val) >> 16), \
-    (unsigned char)(((unsigned long)val) >> 8), \
-    (unsigned char)(((unsigned long)val) >> 0)
+#define STACK_START(typ, var, siz ) typ var[siz]
+#define STACK_END(s)
+#define ADD_TO_STACK4(s,val,offset) \
+    write_unaligned32( &s[offset], (unsigned long)val )
+#define ADD_TO_STACK2(s,val,offset) \
+    write_unaligned16( &s[offset], (unsigned short)val )
+#define ADD_TO_STACK1(s,val,offset) \
+    s[offset] = (unsigned char)val; \
+    s[offset+1] = 0
 
-#define ADD_TO_STACK2(val,offset) \
-    (unsigned char)(((unsigned short)val) >> 8), \
-    (unsigned char)(((unsigned short)val) >> 0)
-
-#define ADD_TO_STACK1(val,offset) \
-    (unsigned char)(val), \
-    (unsigned char)(0)
-
-#define SWAP1_NON_NULL_IN(ptr) if ( !!(ptr) ) { Byte_Swap16(*(ptr)); }
-#define SWAP2_NON_NULL_IN(ptr) if ( !!(ptr) ) { Byte_Swap16(*(ptr)); }
-#define SWAP4_NON_NULL_IN(ptr) if ( !!(ptr) ) { Byte_Swap32(*(ptr)); }
+#define SWAP1_NON_NULL_IN(ptr) if ( !!(ptr) ) { *(ptr) = Byte_Swap16(*(ptr)); }
+#define SWAP2_NON_NULL_IN(ptr) if ( !!(ptr) ) { *(ptr) = Byte_Swap16(*(ptr)); }
+#define SWAP4_NON_NULL_IN(ptr) if ( !!(ptr) ) { *(ptr) = Byte_Swap32(*(ptr)); }
 #define SWAP1_NON_NULL_OUT(ptr) SWAP1_NON_NULL_IN(ptr) 
 #define SWAP2_NON_NULL_OUT(ptr) SWAP2_NON_NULL_IN(ptr) 
 #define SWAP4_NON_NULL_OUT(ptr) SWAP4_NON_NULL_IN(ptr) 
@@ -51,6 +75,18 @@ PNOState* getStorageLoc();
 
 unsigned long Byte_Swap32( unsigned long l );
 unsigned short Byte_Swap16( unsigned short l );
+void write_unaligned16( unsigned char* dest, unsigned short val );
+void write_unaligned32( unsigned char* dest, unsigned long val );
+#define write_unaligned8( p, v ) *(p) = v
+
+
+#if 0
+# define EMIT_NAME(n) \
+    asm( ".byte " n ); \
+    asm( ".align" )
+#else
+# define EMIT_NAME(n)
+#endif
 
 #endif
 #endif
