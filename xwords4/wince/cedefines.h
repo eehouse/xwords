67a5554d99974fb6280aba39d0229a6d8f4b4c84
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2004-12-17 15:08:00 +0000

    move scoreboard back to top

--- xwords4/wince/cedefines.h
+++ xwords4/wince/cedefines.h
@@ -20,60 +20,51 @@
 #ifndef _CEDEFINES_H_
 #define _CEDEFINES_H_
 
-#if 0                          /* Palm-like case */
-
-#define CE_BOARD_SCALEH 14
-#define CE_BOARD_SCALEV 14
-#define CE_MAX_ROWS 15
-#define CE_BOARD_LEFT_LH 8
-#define CE_BOARD_LEFT_RH 1
-#define CE_SCORE_TOP 25
-#define CE_SCORE_HEIGHT 15
-#define CE_BOARD_TOP (CE_SCORE_TOP + CE_SCORE_HEIGHT)
-#define CE_SCORE_LEFT CE_BOARD_LEFT_RH
-#define CE_TRAY_TOP (CE_BOARD_TOP + (CE_MAX_ROWS*CE_BOARD_SCALEV))
-#define CE_TRAY_TOP_MAX CE_TRAY_TOP
-#define CE_BOARD_WIDTH (CE_MAX_ROWS*CE_BOARD_HSCALE)
-
-#define CE_TRAY_LEFT_RH 0
-#define CE_TRAY_LEFT_LH 0
-#define CE_DIVIDER_WIDTH 3
-#define CE_TRAY_SCALEH 31
-#define CE_TRAY_SCALEV CE_TRAY_SCALEH
-
-#define BUTTON_WIDTH 20
-#define BUTTON_HEIGHT BUTTON_WIDTH
-
-#define CE_TIMER_WIDTH 35
-#define CE_TIMER_LEFT (CE_BOARD_WIDTH + BUTTON_WIDTH - CE_TIMER_WIDTH)
-#define CE_TIMER_TOP CE_SCORE_TOP
-#define CE_TIMER_HEIGHT CE_SCORE_HEIGHT
-
-#else  /* tweaked for ce's stupid keyboard button: score on bottom */
-
 #define CE_BOARD_SCALEH 16
 #define CE_BOARD_SCALEV 15
 #define CE_MAX_ROWS 15
 #define CE_BOARD_LEFT_LH 8
 #define CE_BOARD_LEFT_RH 0
-#define CE_BOARD_TOP 0
 #define CE_BOARD_WIDTH (CE_MAX_ROWS*CE_BOARD_SCALEH)
+#define CE_TRAY_LEFT_RH 0
+#define CE_TRAY_LEFT_LH 0
+#define CE_DIVIDER_WIDTH 3
+#define CE_TIMER_WIDTH 35
+#define CE_SCORE_WIDTH (4 * 51)
+
+#if 1                          /* Palm-like case */
+
+#define CE_SCORE_TOP 0
+#define CE_SCORE_HEIGHT 15
+#define CE_BOARD_TOP (CE_SCORE_TOP + CE_SCORE_HEIGHT)
+#define CE_SCORE_LEFT CE_BOARD_LEFT_RH
+#define CE_TRAY_TOP (CE_BOARD_TOP + (CE_MAX_ROWS*CE_BOARD_SCALEV))
+#define CE_TRAY_TOP_MAX CE_TRAY_TOP
+
+#define CE_TRAY_SCALEH 34
+#define CE_TRAY_SCALEV 27
+
+#define BUTTON_WIDTH 20
+#define BUTTON_HEIGHT BUTTON_WIDTH
+
+#define CE_TIMER_LEFT (CE_SCORE_WIDTH + CE_SCORE_LEFT)
+#define CE_TIMER_TOP CE_SCORE_TOP
+#define CE_TIMER_HEIGHT CE_SCORE_HEIGHT
+
+#else  /* tweaked for ce's stupid keyboard button: score on bottom */
+
+#define CE_BOARD_TOP 0
 #define CE_BOARD_HEIGHT (CE_MAX_ROWS*CE_BOARD_SCALEV)
 
 #define CE_TRAY_TOP (CE_BOARD_TOP + CE_BOARD_HEIGHT)
 #define CE_TRAY_SCALEH 34
 #define CE_TRAY_SCALEV 27
 #define CE_TRAY_TOP_MAX CE_TRAY_TOP
-#define CE_TRAY_LEFT_RH 0
-#define CE_TRAY_LEFT_LH 0
-#define CE_DIVIDER_WIDTH 3
 
 #define CE_SCORE_TOP (CE_TRAY_TOP + CE_TRAY_SCALEV)
 #define CE_SCORE_HEIGHT 18
 #define CE_SCORE_LEFT CE_BOARD_LEFT_RH
-#define CE_SCORE_WIDTH (4 * 51)
 
-#define CE_TIMER_WIDTH 30
 #define CE_TIMER_LEFT (CE_SCORE_WIDTH + CE_SCORE_LEFT)
 #define CE_TIMER_RIGHT (CE_TIMER_LEFT + CE_TIMER_WIDTH)
 #define CE_TIMER_TOP CE_SCORE_TOP
