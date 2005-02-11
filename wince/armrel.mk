6a4bd1bb4a873863ab1c72a1a0191d9cc9275c61
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2005-02-11 02:45:59 +0000

    update so works again.  Some optimizations produce bad code in EVC4. Turn them off, and choose others.  This will be the preferred way of building from now on since the files produced by the IDE rearrange their parts at random whenever changed, making diff useless for figuring out what broke.

--- wince/armrel.mk
+++ wince/armrel.mk
@@ -7,25 +7,28 @@
 PLATFORM = ARMV4Rel
 TARGET = $(PLATFORM)/xwords4.exe
 
-LIBS = commctrl.lib coredll.lib winsock.lib
+LIBS = commctrl.lib coredll.lib winsock.lib aygshell.lib
 RSRC = $(PLATFORM)/xwords4.res
 
 MAKE = make -f armrel.mk
+
+CC = $(WCE420)/bin/clarm.exe
+LINK = $(WCE420)/bin/link.exe
+RC = $(VSDIR)/Common/MSDev98/Bin/rc.exe
 
 include ../common/config.mk
 
 include ./shared.mk
 
+
 # Since three rules use exactly the same command, I'm writing it out
 # only once
 C_CMD = \
-	mkdir -p $(dir $@) && \
-	clarm.exe /nologo /W3 /I \
-		"$(WINCE_DEV_PATH)\Include\Armv4" \
+	$(CC) /nologo /W3 /Os /O1 /I "$(WINCE_PATH)\Include\Armv4" \
 		/I "..\common" /I "." /D _WIN32_WCE=420 /D "WIN32_PLATFORM_PSPC=400" \
 		/D "ARM" /D "_ARM_" /D "ARMV4" /D UNDER_CE=420 /D "UNICODE" \
 		/D "_UNICODE" /D "NDEBUG" $(XW_C_DEFINES) \
-		/Fo$@ /O2 /MC /c $<
+		/Fo$@ /MC /c $<
 
 $(PLATFORM)/StdAfx.o: StdAfx.cpp
 	mkdir -p $(dir $@)
@@ -33,29 +36,29 @@ $(PLATFORM)/StdAfx.o: StdAfx.cpp
 
 $(PLATFORM)/%.o: %.c
 	mkdir -p $(dir $@)
-	echo $(pathname $@)
 	$(C_CMD)
 
 ../common/$(PLATFORM)/%.o: ../common/%.c
+	mkdir -p $(dir $@)
 	$(C_CMD)
 
 $(RSRC): xwords4.rc
 	mkdir -p $(PLATFORM)
-	rc.exe /l 0x409 /fo$@ \
-		/i "$(WINCE_DEV_PATH)\Include\Armv4" \
+	$(RC) /l 0x409 /fo$@ \
+		/i "$(WINCE_PATH)\Include\Armv4" \
 		/d UNDER_CE=420 /d _WIN32_WCE=420 /d "NDEBUG" /d "UNICODE" \
 		/d "_UNICODE" /d "WIN32_PLATFORM_PSPC=400" /d "ARM" /d "_ARM_" \
 		/d "ARMV4" $(XW_RES_DEFINES) \
 		/r $<
 
 $(TARGET): $(COMMONOBJ) $(PLATOBJ) $(RSRC)
-	link.exe $(LIBS) /nologo /base:"0x00010000" \
+	$(LINK) $(LIBS) /nologo /base:"0x00010000" \
 		/stack:0x10000,0x1000 /entry:"WinMainCRTStartup" /incremental:no \
 		/pdb:$(basename $(TARGET)).pdb /nodefaultlib:"libc.lib" \
 		/nodefaultlib:libcd.lib /nodefaultlib:libcmt.lib \
 		/nodefaultlib:libcmtd.lib /nodefaultlib:msvcrt.lib \
 		/nodefaultlib:msvcrtd.lib /out:$@ \
-		/libpath:"$(WINCE_PATH)\Lib\Armv4" \
+		/libpath:"$(PPC_SDK_PPC)\Lib\Armv4" \
 		/subsystem:windowsce,4.20 /align:"4096" /MACHINE:ARM \
 		$^
 
