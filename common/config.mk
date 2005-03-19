7d84a2654f705828b7c94d777e672868c0ead345
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2005-03-19 21:47:18 +0000

    added '-I relay/'

--- common/config.mk
+++ common/config.mk
@@ -16,7 +16,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-INCLUDES += -I../common -I./
+COMMON_INCS = -I../common -I../relay
+INCLUDES += $(COMMON_INCS) -I./
 
 COMMONDIR ?= ../common
 COMMONOBJDIR = ../common/$(PLATFORM)
