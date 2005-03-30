99d713bd8ab2ae1339cff48d9c3bf8588063c682
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2005-03-30 01:47:55 +0000

    first checked in

---
+++ xwords4/relay/tpool.h
@@ -0,0 +1,78 @@
+/* -*-mode: C; fill-column: 78; c-basic-offset: 4; -*- */
+
+/* 
+ * Copyright 2005 by Eric House (fixin@peak.org).  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/* Runs a single thread polling for activity on any of the sockets in its
+ * list.  When there is activity, removes the socket from that list and adds
+ * it ot a queue of socket ready for reading.  Starts up a bunch of threads
+ * waiting on that queue.  When a new socket appears, a thread grabs the
+ * socket, reads from it, passes the buffer on, and puts the socket back in
+ * the list being read from in our main thread.
+ */
+
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+class XWThreadPool {
+
+ public:
+    static XWThreadPool* GetTPool();
+    typedef void (*packet_func)( unsigned char* buf,
+                                 int bufLen, int socket );
+    XWThreadPool();
+    ~XWThreadPool();
+
+    void Setup( int nThreads, packet_func pFunc );
+
+    void AddSocket( int socket );
+    void RemoveSocket( int socket );
+
+    void Poll();
+
+ private:
+    void enqueue( int socket );
+    int get_process_packet( int socket );
+    void interrupt_poll();
+
+    void real_tpool_main();
+    static void* tpool_main( void* closure );
+
+    void real_listener();
+    static void* listener_main( void* closure );
+
+    /* Sockets main thread listens on */
+    vector<int> m_activeSockets;
+    pthread_mutex_t m_activeSocketsMutex;
+
+    /* Sockets waiting for a thread to read 'em */
+    deque<int> m_queue;
+    pthread_mutex_t m_queueMutex;
+    pthread_cond_t m_queueCondVar;
+
+    /* for self-write pipe hack */
+    int m_pipeRead;
+    int m_pipeWrite;
+
+    int m_nThreads;
+    packet_func m_pFunc;
+
+    static XWThreadPool* g_instance;
+};
