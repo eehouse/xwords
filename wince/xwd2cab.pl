144e7b408b5a2b5388d9a7f951183d6deb558622
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2006-04-15 23:46:24 +0000

    Seems to work to pack a .xwd file so that it's installed on a device when clicked.  Still haven't figured out how to include info about the app name, developer name etc.

---
+++ wince/xwd2cab.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+# Script for turning Crosswords .xwd files into .cab files that, when
+# run on a PPC device, will install the enclosed .xwd file into a
+# Crosswords directory in "c:\Program Files\Crosswords".
+
+use strict;
+
+my $provider = "-p \"Crosswords project\"";
+
+usage() if 0 == @ARGV;
+
+while ( my $path = shift @ARGV ) {
+
+    unless ( $path =~ m|.xwd$| ) {
+        print STDERR "skipping $path: doesn't end in .xwd\n";
+        next;
+    }
+    unless (-f $path ) {
+        print STDERR "skipping $path: not found\n";
+        next;
+    }
+
+    my $fname = "/tmp/file$$.list";
+
+    open FILE, "> $fname";
+    print FILE "$path ";
+    print FILE '%CE1%\\Crosswords', "\n";
+    close FILE;
+
+    my $cabname = `basename $path`;
+    chomp $cabname;
+    $cabname =~ s/.xwd$/_xwd/;
+    $cabname .= ".cab";
+
+    `pocketpc-cab $provider $fname $cabname`;
+
+    print STDERR "$cabname done\n";
+    unlink $fname;
+}
+
+sub usage() {
+    print STDERR "usage: $0 xwdfile[ xwdfile]*\n";
+    exit 2;
+}
