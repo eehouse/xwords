e68771939ebfae69feb39243e56bcd976f336082
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2006-08-11 01:33:19 +0000

    fix broken ce build: remove ifdefs.

--- xwords4/wince/resource.h
+++ xwords4/wince/resource.h
@@ -169,9 +169,7 @@
 #define ID_MOVE_JUGGLE                  40016
 #define ID_MOVE_HIDETRAY                40017
 #define ID_MOVE_TURNDONE                40018
-#ifndef _WIN32_WCE
-# define ID_MOVE_FLIP                   40019
-#endif
+#define ID_MOVE_FLIP                   40019
 #define ID_FILE_NEWGAME                 40020
 #define ID_FILE_SAVEDGAMES              40021
 #define ID_EDITTEXT                     40022
