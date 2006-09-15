d7a2fadd39f2bd919d360ddb04273459ebb72fb7
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2006-09-15 07:30:00 +0000

    modify MEM_DEBUG versions of strdup utils to record the __FILE__ and __LINE__ of callers rather than their own to assist in leak detection.

--- xwords4/common/strutils.h
+++ xwords4/common/strutils.h
@@ -41,9 +41,31 @@ XP_UCHAR* stringFromStream( MPFORMAL XWStreamCtxt* stream );
 XP_U16 stringFromStreamHere( XWStreamCtxt* stream, XP_UCHAR* buf, XP_U16 len );
 void stringToStream( XWStreamCtxt* stream, XP_UCHAR* str );
 
-XP_UCHAR* copyString( MPFORMAL const XP_UCHAR* instr );
-void replaceStringIfDifferent( MPFORMAL XP_UCHAR** curLoc, 
-                               const XP_UCHAR* newStr );
+XP_UCHAR* p_copyString( MPFORMAL const XP_UCHAR* instr 
+#ifdef MEM_DEBUG
+                        , const char* file, XP_U32 lineNo 
+#endif
+                      );
+#ifdef MEM_DEBUG
+# define copyString( p, in ) p_copyString( (p), (in), __FILE__, __LINE__ )
+#else
+# define copyString( p, in ) p_copyString( in )
+#endif
+
+
+void p_replaceStringIfDifferent( MPFORMAL XP_UCHAR** curLoc, 
+                                 const XP_UCHAR* newStr
+#ifdef MEM_DEBUG
+                                 , const char* file, XP_U32 lineNo 
+#endif
+                               );
+#ifdef MEM_DEBUG
+# define replaceStringIfDifferent(p, sp, n) \
+         p_replaceStringIfDifferent( (p), (sp), (n), __FILE__, __LINE__ )
+#else
+# define replaceStringIfDifferent(p, sp, n) p_replaceStringIfDifferent((sp),(n))
+#endif
+
 
 XP_UCHAR* emptyStringIfNull( XP_UCHAR* str );
 
