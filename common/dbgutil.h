02b45a156ce75929a414f692085dae8f40748a4b
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2006-11-03 06:19:27 +0000

    debug functions to turn enums and defines into loggable strings

---
+++ common/dbgutil.h
@@ -0,0 +1,29 @@
+/* -*-mode: C; fill-column: 78; c-basic-offset: 4; -*- */
+/* 
+ * Copyright 2006 by Eric House (xwords@eehouse.org).  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef _DBGUTIL_H_
+#define _DBGUTIL_H_
+
+#include "board.h"
+
+const char* XP_Key_2str( XP_Key key );
+const char* DrawFocusState_2str( DrawFocusState dfs );
+const char* BoardObjectType_2str( BoardObjectType dfs );
+
+#endif
