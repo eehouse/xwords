23b85f54379d62da44f86b4c07cbd40c5e97637d
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2006-12-13 05:03:19 +0000

    BT work is now 4.3.  Nav work moves into beta as 4.2.

--- xwords4/palm/xwords4defines.h
+++ xwords4/palm/xwords4defines.h
@@ -422,7 +422,11 @@
 #define	kFrmNavHeaderFlagsAppFocusStartState     0x00000002
 
 /* versioning stuff */
-#define XW_PALM_VERSION_STRING "4.2a3"
+#ifdef XWFEATURE_BLUETOOTH
+# define XW_PALM_VERSION_STRING "4.3a4"
+#else
+# define XW_PALM_VERSION_STRING "4.2b1"
+#endif
 #define CUR_PREFS_VERS 0x0405
 
 
