51d4c608f93ed372e75e5784bd848deb1d12d152
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2008-05-12 02:51:22 +0000

    Don't include mingwm10.dll in .cab.  Doesn't seem to be needed.

--- xwords4/wince/exe2cab.pl
+++ xwords4/wince/exe2cab.pl
@@ -49,8 +49,8 @@ sub main() {
     print FILE "../dawg/English/BasEnglish2to8.xwd ";
     print FILE '%CE1%\\Crosswords', "\n";
 
-    print FILE "$ENV{'CEOPT_ROOT'}/opt/mingw32ce/arm-wince-mingw32ce/bin/mingwm10.dll ";
-    print FILE '%CE2%\\mingwm10.dll', "\n";
+#     print FILE "$ENV{'CEOPT_ROOT'}/opt/mingw32ce/arm-wince-mingw32ce/bin/mingwm10.dll ";
+#     print FILE '%CE2%\\mingwm10.dll', "\n";
 
     print FILE "$linkName ";
     print FILE '%CE14%', "\n";
