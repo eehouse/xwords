0fd6eb0aeaca1b1b5418055d7d72574a7aa91f2f
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2008-12-31 04:26:37 +0000

    tweaks to stop/start script to run on an ISP's server better

--- relay/xwrelay.sh
+++ relay/xwrelay.sh
@@ -1,41 +1,59 @@
 #!/bin/sh
 
-XWRELAY="./xwrelay"
-PIDFILE=./xwrelay.pid
+DIR=${DIR:-$(dirname $0)}
+XWRELAY=${DIR}/xwrelay
+PIDFILE=${DIR}/xwrelay.pid
+CONFFILE=${DIR}/xwrelay.conf
+IDFILE=${DIR}/nextid.txt
 
-CMD=$1
-shift
+do_start() {
+    if [ -f $PIDFILE ] && [ -f /proc/$(cat $PIDFILE)/exe ]; then
+        echo "already running: pid=$(cat $PIDFILE)"
+    elif pidof xwrelay >/dev/null; then
+        echo "already running: pid=$(pidof xwrelay)"
+    else
+        echo "starting..."
+        echo "running $XWRELAY $@ -f $CONFFILE"
+        $XWRELAY $@ -f $CONFFILE -i $IDFILE &
+        NEWPID=$!                
+        echo $NEWPID > $PIDFILE
+        sleep 1
+        echo "running with pid=$(cat $PIDFILE)"
+    fi
+}
 
-case $CMD in
+case $1 in
     
     stop)
+        shift
         if [ -f $PIDFILE ] && [ -f /proc/$(cat $PIDFILE)/exe ]; then
             sync
             echo "killing pid=$(cat $PIDFILE)"
             kill $(cat $PIDFILE)
         else
-            echo "not running"
+            echo "not running or $PIDFILE not found"
+            PID=$(pidof xwrelay || true)
+            if [ "x${PID}" != "x" ]; then
+                echo "maybe it's $PID; killing them"
+                kill $PID
+            fi
         fi
-        rm $PIDFILE
+        rm -f $PIDFILE
         ;;
 
     restart)
+        shift
         $0 stop
         sleep 1
         $0 start $@
         ;;
 
-    start|*)
-        if [ -f $PIDFILE ] && [ -f /proc/$(cat $PIDFILE)/exe ]; then
-            echo "already running: pid=$(cat $PIDFILE)"
-        else
-            echo "starting..."
-            $XWRELAY $@ &
-            NEWPID=$!
-            echo $NEWPID > $PIDFILE
-            sleep 1
-            echo "running with pid=$(cat $PIDFILE)"
-        fi
+    start)
+        shift
+        do_start $@
+        ;;
+    *)
+        do_start $@
         ;;
 
 esac
