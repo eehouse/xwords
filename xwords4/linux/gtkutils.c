bac17d8c1e097ca4487db20501654e9fc1916eca
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2009-02-24 04:34:37 +0000

    start adding conns dialog

---
+++ xwords4/linux/gtkutils.c
@@ -0,0 +1,52 @@
+/* -*-mode: C; fill-column: 78; c-basic-offset: 4; compile-command: "make MEMDEBUG=TRUE"; -*- */
+/* 
+ * Copyright 2001-2008 by Eric House (xwords@eehouse.org).  All rights
+ * reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+#ifdef PLATFORM_GTK
+
+#include "gtkutils.h"
+
+GtkWidget*
+makeButton( char* text, GCallback func, gpointer data )
+{
+    GtkWidget* button = gtk_button_new_with_label( text );
+    g_signal_connect( GTK_OBJECT(button), "clicked", func, data );
+    gtk_widget_show( button );
+
+    return button;
+} /* makeButton */
+
+
+GtkWidget*
+makeLabeledField( const char* labelText, GtkWidget** field )
+{
+    char buf[64];
+    snprintf( buf, sizeof(buf), "%s:", labelText );
+    GtkWidget* label = gtk_label_new( buf );
+
+    *field = gtk_entry_new();
+
+    GtkWidget* hbox = gtk_hbox_new( FALSE, 0 );
+    gtk_box_pack_start( GTK_BOX(hbox), label, FALSE, TRUE, 0 );
+    gtk_box_pack_start( GTK_BOX(hbox), *field, FALSE, TRUE, 0 );
+    return hbox;
+}
+
+#endif
