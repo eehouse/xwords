9cb1f8ab594709b44b283291af5a966cd594f5cc
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2009-02-25 03:45:14 +0000

    minimal test for comparing cegcc versions

---
+++ xwords4/wince/test/messagebox.c
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+int
+WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine,
+         int nCmdShow )
+{
+    MessageBox(0, L"HELLO!", L"H3LLO!", 0);
+    return 0;
+}
