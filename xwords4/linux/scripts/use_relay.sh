13415e895e5423654a2ad5a54b5079f8e5bc813f
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2009-02-28 16:31:34 +0000

    test script, first draft.

---
+++ xwords4/linux/scripts/use_relay.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+COOKIE=${RANDOM}
+
+./obj_linux_memdbg/xwords -d dict.xwd -r Brynn -a localhost \
+    -p 10999 -C $COOKIE &
+./obj_linux_memdbg/xwords -d dict.xwd -r Ariela -a localhost \
+    -p 10999 -C $COOKIE &
+./obj_linux_memdbg/xwords -d dict.xwd -r Kati -a localhost \
+    -p 10999 -C $COOKIE &
+./obj_linux_memdbg/xwords -d dict.xwd -r Eric -s -N -N -N -a localhost \
+    -p 10999 -C $COOKIE &
