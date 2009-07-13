fed0458f2e4b0f5948c74f949d896911d35da982
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2009-07-13 02:57:41 +0000

    various improvements.  Runs games forever now, though exit is clumsy.

--- xwords4/linux/scripts/sim_real.sh
+++ xwords4/linux/scripts/sim_real.sh
@@ -7,6 +7,9 @@ HOST=${HOST:-localhost}
 PORT=${PORT:-10999}
 QUIT=${QUIT:-"-q 2"}
 USE_CURSES=${USE_CURSES:="yes"}
+WAIT_MAX=${WAIT_MAX:-10}
+
+RUN_NAME=$(basename $0)_$$
 
 usage() {
     echo "usage: $0 <no params>"
@@ -19,6 +22,7 @@ EOF
     echo "    env: DICT: dictionary; default: dict.xwd"
     echo "    env: HOST: remote host; default: localhost"
     echo "    env: PORT: remote port; default: 10999"
+    echo "    env: WAIT_MAX: most seconds to wait between moves; default: 10"
     exit 0
 }
 
@@ -30,10 +34,36 @@ random() {
     echo $RANDOM
 }
 
+game_curses() {
+    NAME=$1
+    COOKIE=$2
+    WAIT=$3
+    INDEX=$4
+    SERVER_PARAMS=$5
+    $XWORDS -u -d $DICT -r $NAME -a $HOST -p $PORT \
+        -C $COOKIE $QUIT -z 0:$WAIT >/dev/null -0 $SERVER_PARAMS \
+        2>/tmp/$RUN_NAME/log_${COOKIE}_${INDEX}.txt < /dev/null &
+}
+
+check_logs() {
+    COOKIE=$1
+    OK=1
+    for LOG in /tmp/$RUN_NAME/log_${COOKIE}_*.txt; do
+        if ! grep -q XWPROTO_END_GAME $LOG; then
+            echo "$LOG didn't end correctly; check it out."
+            OK=0
+        fi
+    done
+
+    [ ! 1 = $OK ] && echo "game $COOKIE ended successfully"
+}
+
 do_one() {
     COOKIE=${1:-$(exec sh -c 'echo $PPID')}
 
-    while :; do                 # loop forever (eventually)
+    while [ -d /tmp/$RUN_NAME ]; do                 # loop forever
+
+        unset ZERO_DONE ONE_DONE TWO_DONE THREE_DONE
 
         TODO=$(($COOKIE % 3))
         TODO=$((TODO+2))
@@ -43,7 +73,7 @@ do_one() {
             while :; do
                 RAND=$(random)
                 INDEX=$(( $RAND % $TODO ))
-                WAIT=$(( $RAND % 60 ))
+                WAIT=$(( $RAND % $WAIT_MAX ))
                 case $INDEX in
                     0)
                         if [ -z "$ZERO_DONE" ]; then
@@ -52,13 +82,9 @@ do_one() {
                                 REMOTES="$REMOTES -N"; 
                             done
                             ZERO_DONE=1
-                            echo $COOKIE:ZERO
                             if [ "$USE_CURSES" = "yes" ]; then
-                                $XWORDS -d $DICT -r $NAME -s $REMOTES \
-                                    -a $HOST -p $PORT -C $COOKIE $QUIT -u \
-                                    -z 0:$WAIT >/dev/null \
-                                    2>/tmp/log_${COOKIE}_${INDEX}.txt &
-
+                                game_curses $NAME $COOKIE $WAIT $INDEX \
+                                    "-s $REMOTES"
                             else
                                 $XWORDS -d $DICT -r $NAME -s $REMOTES \
                                     -a $HOST -p $PORT -C $COOKIE $QUIT &
@@ -69,12 +95,8 @@ do_one() {
                     1)
                         if [ -z "$ONE_DONE" ]; then
                             ONE_DONE=1
-                            echo $COOKIE:ONE
                             if [ "$USE_CURSES" = "yes" ]; then
-                                $XWORDS -d $DICT -r $NAME -a $HOST -p $PORT \
-                                    -C $COOKIE $QUIT -u \
-                                    -z 0:$WAIT >/dev/null \
-                                    2>/tmp/log_$COOKIE_${INDEX}.txt &
+                                game_curses $NAME $COOKIE $WAIT $INDEX
                             else
                                 $XWORDS -d $DICT -r $NAME -a $HOST -p $PORT \
                                     -C $COOKIE $QUIT &
@@ -85,13 +107,8 @@ do_one() {
                     2)
                         if [ -z "$TWO_DONE" ]; then
                             TWO_DONE=1
-                            echo $COOKIE:TWO
                             if [ "$USE_CURSES" = "yes" ]; then
-                                $XWORDS -d $DICT -r $NAME -a $HOST -p $PORT \
-                                    -C $COOKIE $QUIT -u \
-                                    -z 0:$WAIT >/dev/null \
-                                    2>/tmp/log_$COOKIE_${INDEX}.txt &
-
+                                game_curses $NAME $COOKIE $WAIT $INDEX
                             else
                                 $XWORDS -d $DICT -r $NAME -a $HOST -p $PORT \
                                     -C $COOKIE $QUIT &
@@ -102,12 +119,8 @@ do_one() {
                     3)
                         if [ -z "$THREE_DONE" ]; then
                             THREE_DONE=1
-                            echo $COOKIE:THREE
                             if [ "$USE_CURSES" = "yes" ]; then
-                                $XWORDS -d $DICT -r $NAME -a $HOST -p $PORT \
-                                    -C $COOKIE $QUIT -u \
-                                    -z 0:$WAIT >/dev/null \
-                                    2>/tmp/log_$COOKIE_${INDEX}.txt &
+                                game_curses $NAME $COOKIE $WAIT $INDEX
                             else
                                 $XWORDS -d $DICT -r $NAME -a $HOST -p $PORT \
                                     -C $COOKIE $QUIT &
@@ -119,8 +132,11 @@ do_one() {
             done
             COUNT=$((COUNT+1))
         done
+
         wait
-        break
+
+        check_logs $COOKIE
+        sleep $(( $(random) % 60 ))
     done
 }
 
@@ -128,6 +144,11 @@ do_one() {
 # main
 
 [ -n "$1" ] && usage
+
+mkdir -p /tmp/$RUN_NAME
+echo "************************************************************"
+echo "* created /tmp/$RUN_NAME; delete it to stop runaway script *"
+echo "************************************************************"
 
 for II in $(seq $NRUNS); do
     do_one $II &
