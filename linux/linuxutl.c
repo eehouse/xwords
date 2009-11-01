b7320ec75af5ac85cb22995919bc2bbc706668bf
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2009-11-01 01:40:18 +0000

    add .2 for consistent time formatting

--- linux/linuxutl.c
+++ linux/linuxutl.c
@@ -40,7 +40,7 @@ linux_debugf( const char* format, ... )
     gettimeofday( &tv, &tz );
     timp = localtime( &tv.tv_sec );
 
-    sprintf( buf, "<%d>%d:%d:%d: ", getpid(), 
+    snprintf( buf, sizeof(buf), "<%d>%.2d:%.2d:%.2d:", getpid(), 
              timp->tm_hour, timp->tm_min, timp->tm_sec );
 
     va_start(ap, format);
