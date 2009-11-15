11a00ddb9be5a0f61fffbfc18da9ce7648a283c3
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2009-11-15 15:48:35 +0000

    Remove Rem string and up dll version number.

--- xwords4/wince/strids.h
+++ xwords4/wince/strids.h
@@ -12,7 +12,7 @@
  * whether a given resource can be trusted.  Up it when changing the order of
  * strings.  Which do only when unavoidable.
  */
-#define CUR_DLL_VERSION 0x0001
+#define CUR_DLL_VERSION 0x0002
 
 
 #define CE_FIRST_RES_ID                                              40002
@@ -34,102 +34,101 @@
 #define IDS_TURN_SCORE                                               40016
 #define IDS_COMMIT_CONFIRM                                           40017
 #define IDS_LOCAL_NAME                                               40018
-#define IDS_REM                                                      40019
-#define IDS_IGNORE_L                                                 40020
-#define IDS_WARN_L                                                   40021
-#define IDS_DISALLOW_L                                               40022
-#define IDS_NONLOCAL_NAME                                            40023
-#define IDS_TIME_PENALTY_SUB                                         40024
-#define IDS_CUMULATIVE_SCORE                                         40025
-#define IDS_MOVE_ACROSS                                              40026
-#define IDS_MOVE_DOWN                                                40027
-#define IDS_TRAY_AT_START                                            40028
-#define IDS_NEW_TILES                                                40029
-#define IDS_TRADED_FOR                                               40030
-#define IDS_PASS                                                     40031
-#define IDS_PHONY_REJECTED                                           40032
-#define IDS_ROBOT_TRADED                                             40033
-#define IDS_ROBOT_MOVED                                              40034
-#define IDS_REMOTE_MOVED                                             40035
-#define IDS_PASSED                                                   40036
-#define IDS_REMTILES_L                                               40037
-#define IDS_SUMMARYSCORED                                            40038
-#define IDS_TRADED                                                   40039
-#define IDS_LOSTTURN                                                 40040
-#define IDS_TOTALPLAYERS                                             40041
-#define IDS_VALUES_HEADER                                            40042
-#define IDS_TILES_NOT_IN_LINE                                        40043
-#define IDS_NO_EMPTIES_IN_TURN                                       40044
-#define IDS_TWO_TILES_FIRST_MOVE                                     40045
-#define IDS_TILES_MUST_CONTACT                                       40046
-#define IDS_NOT_YOUR_TURN                                            40047
-#define IDS_NO_PEEK_ROBOT_TILES                                      40048
-#define IDS_CANT_TRADE_MID_MOVE                                      40049
-#define IDS_TOO_FEW_TILES_LEFT_TO_TRADE                              40050
-#define IDS_CANT_UNDO_TILEASSIGN                                     40051
-#define IDS_CANT_HINT_WHILE_DISABLED                                 40052
-#define IDS_QUERY_TRADE                                              40053
-#define IDS_DOUBLE_LETTER                                            40054
-#define IDS_DOUBLE_WORD                                              40055
-#define IDS_TRIPLE_LETTER                                            40056
-#define IDS_TRIPLE_WORD                                              40057
-#define IDS_INTRADE_MW                                               40058
-#define IDS_COUNTSVALS_L                                             40059
-#define IDS_GAMEHIST_L                                               40060
-#define IDS_FINALSCORE_L                                             40061
-#define IDS_QUESTION_L                                               40062
-#define IDS_FYI_L                                                    40063
-#define IDS_ILLEGALWRD_L                                             40064
-#define IDS_WRDNOTFOUND                                              40065
-#define IDS_USEANYWAY                                                40066
-#define IDS_CANNOTOPEN_GAME                                          40067
-#define IDS_NODICT_L                                                 40068
-#define IDS_ABOUT_L                                                  40069
-#define IDS_OVERWRITE                                                40070
-#define IDS_ENDNOW                                                   40071
-#define IDS_CANNOTOPEN_DICT                                          40072
-#define IDS_CONFIM_DELETE                                            40073
-#define IDS_ROLE_STANDALONE                                          40074
-#define IDS_ROLE_HOST                                                40075
-#define IDS_ROLE_GUEST                                               40076
-#define IDS_PLAYER_FORMAT                                            40077
-#define IDS_UNTITLED_FORMAT                                          40078
-#define IDS_PASSWDFMT_L                                              40079
-#define IDS_FILEEXISTSFMT_L                                          40080
-#define IDS_NEED_TOUCH                                               40081
-#define IDS_EDITCOLOR_FORMAT                                         40082
-#define IDS_LANG_CHANGE_RESTART                                      40083
+#define IDS_IGNORE_L                                                 40019
+#define IDS_WARN_L                                                   40020
+#define IDS_DISALLOW_L                                               40021
+#define IDS_NONLOCAL_NAME                                            40022
+#define IDS_TIME_PENALTY_SUB                                         40023
+#define IDS_CUMULATIVE_SCORE                                         40024
+#define IDS_MOVE_ACROSS                                              40025
+#define IDS_MOVE_DOWN                                                40026
+#define IDS_TRAY_AT_START                                            40027
+#define IDS_NEW_TILES                                                40028
+#define IDS_TRADED_FOR                                               40029
+#define IDS_PASS                                                     40030
+#define IDS_PHONY_REJECTED                                           40031
+#define IDS_ROBOT_TRADED                                             40032
+#define IDS_ROBOT_MOVED                                              40033
+#define IDS_REMOTE_MOVED                                             40034
+#define IDS_PASSED                                                   40035
+#define IDS_REMTILES_L                                               40036
+#define IDS_SUMMARYSCORED                                            40037
+#define IDS_TRADED                                                   40038
+#define IDS_LOSTTURN                                                 40039
+#define IDS_TOTALPLAYERS                                             40040
+#define IDS_VALUES_HEADER                                            40041
+#define IDS_TILES_NOT_IN_LINE                                        40042
+#define IDS_NO_EMPTIES_IN_TURN                                       40043
+#define IDS_TWO_TILES_FIRST_MOVE                                     40044
+#define IDS_TILES_MUST_CONTACT                                       40045
+#define IDS_NOT_YOUR_TURN                                            40046
+#define IDS_NO_PEEK_ROBOT_TILES                                      40047
+#define IDS_CANT_TRADE_MID_MOVE                                      40048
+#define IDS_TOO_FEW_TILES_LEFT_TO_TRADE                              40049
+#define IDS_CANT_UNDO_TILEASSIGN                                     40050
+#define IDS_CANT_HINT_WHILE_DISABLED                                 40051
+#define IDS_QUERY_TRADE                                              40052
+#define IDS_DOUBLE_LETTER                                            40053
+#define IDS_DOUBLE_WORD                                              40054
+#define IDS_TRIPLE_LETTER                                            40055
+#define IDS_TRIPLE_WORD                                              40056
+#define IDS_INTRADE_MW                                               40057
+#define IDS_COUNTSVALS_L                                             40058
+#define IDS_GAMEHIST_L                                               40059
+#define IDS_FINALSCORE_L                                             40060
+#define IDS_QUESTION_L                                               40061
+#define IDS_FYI_L                                                    40062
+#define IDS_ILLEGALWRD_L                                             40063
+#define IDS_WRDNOTFOUND                                              40064
+#define IDS_USEANYWAY                                                40065
+#define IDS_CANNOTOPEN_GAME                                          40066
+#define IDS_NODICT_L                                                 40067
+#define IDS_ABOUT_L                                                  40068
+#define IDS_OVERWRITE                                                40069
+#define IDS_ENDNOW                                                   40070
+#define IDS_CANNOTOPEN_DICT                                          40071
+#define IDS_CONFIM_DELETE                                            40072
+#define IDS_ROLE_STANDALONE                                          40073
+#define IDS_ROLE_HOST                                                40074
+#define IDS_ROLE_GUEST                                               40075
+#define IDS_PLAYER_FORMAT                                            40076
+#define IDS_UNTITLED_FORMAT                                          40077
+#define IDS_PASSWDFMT_L                                              40078
+#define IDS_FILEEXISTSFMT_L                                          40079
+#define IDS_NEED_TOUCH                                               40080
+#define IDS_EDITCOLOR_FORMAT                                         40081
+#define IDS_LANG_CHANGE_RESTART                                      40082
 
 #ifndef XWFEATURE_STANDALONE_ONLY
-# define IDS_CONN_RELAY_L                                            40084
-# define IDS_CONN_DIRECT                                             40085
-# define IDS_LOCALPLAYERS                                            40086
-# define IDS_NO_PEEK_REMOTE_TILES                                    40087
-# define IDS_REG_UNEXPECTED_USER                                     40088
-# define IDS_SERVER_DICT_WINS                                        40089
-# define IDS_REG_SERVER_SANS_REMOTE                                  40090
-# define IDS_RESEND_STANDALONE                                       40091
+# define IDS_CONN_RELAY_L                                            40083
+# define IDS_CONN_DIRECT                                             40084
+# define IDS_LOCALPLAYERS                                            40085
+# define IDS_NO_PEEK_REMOTE_TILES                                    40086
+# define IDS_REG_UNEXPECTED_USER                                     40087
+# define IDS_SERVER_DICT_WINS                                        40088
+# define IDS_REG_SERVER_SANS_REMOTE                                  40089
+# define IDS_RESEND_STANDALONE                                       40090
 
 # ifdef XWFEATURE_SMS
-#  define IDS_SMS_CONN_L                                             40092
+#  define IDS_SMS_CONN_L                                             40091
 # endif
 
 # ifdef XWFEATURE_IP_DIRECT
-#  define IDS_DIRECT_CONN_L                                          40093
+#  define IDS_DIRECT_CONN_L                                          40092
 # endif
 
 # ifdef XWFEATURE_RELAY
-#  define IDS_XWRELAY_ERROR_TIMEOUT                                  40094
-#  define IDS_ERROR_HEART_YOU                                        40095
-#  define IDS_XWRELAY_ERROR_HEART_OTHER                              40096
-#  define IDS_XWRELAY_ERROR_LOST_OTHER                               40097
+#  define IDS_XWRELAY_ERROR_TIMEOUT                                  40093
+#  define IDS_ERROR_HEART_YOU                                        40094
+#  define IDS_XWRELAY_ERROR_HEART_OTHER                              40095
+#  define IDS_XWRELAY_ERROR_LOST_OTHER                               40096
 # endif
 #endif
 
 #if ! defined XWFEATURE_STANDALONE_ONLY
-# define CE_LAST_RES_ID                                              40098
+# define CE_LAST_RES_ID                                              40097
 #else
-# define CE_LAST_RES_ID                                              40083
+# define CE_LAST_RES_ID                                              40082
 #endif
 
 #endif
