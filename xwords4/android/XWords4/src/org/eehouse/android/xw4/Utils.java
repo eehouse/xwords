531ae8ea1a59fee082441dcc4b8baa600ee0071c
ehouse <ehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2010-01-20 06:35:56 +0000

    add file open/save functions.

--- xwords4/android/XWords4/src/org/eehouse/android/xw4/Utils.java
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/Utils.java
@@ -7,6 +7,8 @@ import java.lang.Thread;
 import java.text.MessageFormat;
 import android.widget.Toast;
 import android.content.Context;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
 
 public class Utils {
     static final String TAG = "EJAVA";
@@ -34,5 +36,38 @@ public class Utils {
         CharSequence text = "Version: pre-alpha; svn rev: " + SvnVersion.VERS;
         Toast.makeText( context, text, Toast.LENGTH_LONG ).show();
     }
+
+    public static byte[] savedGame( Context context, String path )
+    {
+        byte[] stream = null;
+        try {
+            FileInputStream in = context.openFileInput( path );
+            int len = in.available();
+            Utils.logf( "savedGame: got " + len + " bytes." );
+            stream = new byte[len];
+            in.read( stream, 0, len );
+            in.close();
+        } catch ( java.io.FileNotFoundException fnf ) {
+            Utils.logf( fnf.toString() );
+            stream = null;
+        } catch ( java.io.IOException io ) {
+            Utils.logf( io.toString() );
+            stream = null;
+        }
+        return stream;
+    } // savedGame
+
+
+    public static void saveGame( Context context, byte[] bytes, String path )
+    {
+        try {
+            FileOutputStream out = context.openFileOutput( path, Context.MODE_PRIVATE );
+            out.write( bytes );
+            out.close();
+        } catch ( java.io.IOException ex ) {
+            Utils.logf( "got IOException: " + ex.toString() );
+        }
+    }
+
 
 }
