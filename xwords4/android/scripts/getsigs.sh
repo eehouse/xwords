0396041864dd7ff6ca25eb846c1e12c6e0bce06c
eehouse <eehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2010-02-14 17:29:07 +0000

    search TransportProcs too

--- xwords4/android/scripts/getsigs.sh
+++ xwords4/android/scripts/getsigs.sh
@@ -7,5 +7,6 @@ javah -o /tmp/javah$$.txt org.eehouse.android.xw4.jni.XwJNI
 javap -s org.eehouse.android.xw4.jni.DrawCtx
 javap -s org.eehouse.android.xw4.jni.XW_UtilCtxt
 javap -s org.eehouse.android.xw4.jni.CommsAddrRec
+javap -s org.eehouse.android.xw4.jni.TransportProcs
 cat /tmp/javah$$.txt
 rm /tmp/javah$$.txt
