3d26d9fc01fe38a5368bb273b119a47022eb3c8e
eehouse <eehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2010-02-24 04:28:52 +0000

    name of file has changed

--- xwords4/android/scripts/getsigs.sh
+++ xwords4/android/scripts/getsigs.sh
@@ -5,7 +5,7 @@ cd $BASE/../XWords4/bin/classes
 
 javah -o /tmp/javah$$.txt org.eehouse.android.xw4.jni.XwJNI
 javap -s org.eehouse.android.xw4.jni.DrawCtx
-javap -s org.eehouse.android.xw4.jni.XW_UtilCtxt
+javap -s org.eehouse.android.xw4.jni.UtilCtxt
 javap -s org.eehouse.android.xw4.jni.CommsAddrRec
 javap -s org.eehouse.android.xw4.jni.TransportProcs
 cat /tmp/javah$$.txt
