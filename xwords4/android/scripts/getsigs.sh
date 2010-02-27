856b957f6a317eae38a0053a1faff9681e3fbbdf
eehouse <eehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2010-02-27 18:05:21 +0000

    another file to search

--- xwords4/android/scripts/getsigs.sh
+++ xwords4/android/scripts/getsigs.sh
@@ -4,6 +4,7 @@ BASE=$(dirname $0)
 cd $BASE/../XWords4/bin/classes
 
 javah -o /tmp/javah$$.txt org.eehouse.android.xw4.jni.XwJNI
+javap -s org.eehouse.android.xw4.jni.XwJNI
 javap -s org.eehouse.android.xw4.jni.DrawCtx
 javap -s org.eehouse.android.xw4.jni.UtilCtxt
 javap -s org.eehouse.android.xw4.jni.CommsAddrRec
