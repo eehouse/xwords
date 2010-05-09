77f21c6f2eb6f245edb90401d05c4275b0370767
eehouse <eehouse@0782aaa5-4710-0410-8820-a96bf9123855>
2010-05-09 14:42:19 +0000

    try passing application context to CommonPrefs.  I'm getting a crash on device when app's been running for a while and, perhaps, the original GamesList activity has been recycled but CommonPrefs instance with its static has not.  The BoardView is being recreated and CommonPrefs asserts that m_context is null.  That shouldn't be possible, but we'll try this.

--- xwords4/android/XWords4/src/org/eehouse/android/xw4/GamesList.java
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/GamesList.java
@@ -108,7 +108,7 @@ public class GamesList extends ListActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
 
         PreferenceManager.setDefaultValues( this, R.xml.xwprefs, false );
-        CommonPrefs.setContext( this );
+        CommonPrefs.setContext( getApplicationContext() );
 
         setContentView(R.layout.game_list);
 
