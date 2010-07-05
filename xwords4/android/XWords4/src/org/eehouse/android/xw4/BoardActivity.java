210a3f033be98163c504a16f0acb8658f2b2af31
Andy2 <andy2@trand.private.network>
2010-07-05 09:52:19 -0700

    disable hint buttons when hint feature's turned off

--- xwords4/android/XWords4/src/org/eehouse/android/xw4/BoardActivity.java
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/BoardActivity.java
@@ -772,6 +772,12 @@ public class BoardActivity extends Activity implements UtilCtxt {
                                                            .CMD_UNDO_CUR );
                                    }
                                }) ;
+
+        // Stuff we can disable now
+        if ( m_gi.hintsNotAllowed ) {
+            m_toolbar.update( Toolbar.BUTTON_HINT_PREV, 0 );
+            m_toolbar.update( Toolbar.BUTTON_HINT_NEXT, 0 );
+        }
     } // populateToolbar
 
     private DialogInterface.OnDismissListener makeODLforBlocking()
