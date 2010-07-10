d1c7d4097c28ee5d93fd28097474d1241dbb29aa
Andy2 <andy2@trand.private.network>
2010-07-09 18:52:14 -0700

    remove focus-related code and declarations

--- xwords4/android/XWords4/src/org/eehouse/android/xw4/jni/XwJNI.java
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/jni/XwJNI.java
@@ -146,8 +146,6 @@ public class XwJNI {
     public static native boolean board_beginTrade( int gamePtr );
 
     public static native String board_formatRemainingTiles( int gamePtr );
-    public static native int board_getFocusOwner( int gamePtr );
-    public static native boolean board_focusChanged( int gamePtr, int typ );
     public static native int board_visTileCount( int gamePtr );
     public static native boolean board_canHint( int gamePtr );
     public static native boolean board_canShuffle( int gamePtr );
