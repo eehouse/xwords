d44b08b60985140928b7468cac3cd69313bba5fa
Andy2 <andy2@trand.private.network>
2010-08-11 18:51:33 -0700

    remove unused and confusing duplicate flags.

--- xwords4/android/XWords4/src/org/eehouse/android/xw4/jni/DrawScoreInfo.java
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/jni/DrawScoreInfo.java
@@ -20,20 +20,6 @@
 package org.eehouse.android.xw4.jni;
 
 public class DrawScoreInfo {
-
-    public static final int CELL_NONE = 0x00;
-    public static final int CELL_ISBLANK = 0x01;
-    public static final int  CELL_HIGHLIGHT = 0x02;
-    public static final int  CELL_ISSTAR = 0x04;
-    public static final int  CELL_ISCURSOR = 0x08;
-    public static final int  CELL_ISEMPTY = 0x10;       /* of a tray tile slot */
-    public static final int  CELL_VALHIDDEN = 0x20;     /* show letter only, not value */
-    public static final int  CELL_DRAGSRC = 0x40;       /* where drag originated */
-    public static final int  CELL_DRAGCUR = 0x80;       /* where drag is now */
-    public static final int  CELL_ALL = 0xFF;
-
-    // LastScoreCallback lsc;
-    // void* lscClosure;
     public String name;
     public int playerNum;
     public int totalScore;
