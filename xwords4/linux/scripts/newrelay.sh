076b211cab9a690bd44635ab06944866a3e7dd02
Eric House <eehouse@eehouse.org>
2010-09-21 20:38:54 -0700

    tweak to allow to specify host/port for use against remote relay

--- xwords4/linux/scripts/newrelay.sh
+++ xwords4/linux/scripts/newrelay.sh
@@ -5,6 +5,8 @@ COOKIE=${COOKIE:-foo}
 NGAMES=${NGAMES:-1}
 NPLAYERS=${NPLAYERS:-2}
 USE_GTK=${USE_GTK:-FALSE}
+HOST=${HOST:-localhost}
+PORT=${PORT:-10997}
 
 [ $USE_GTK = FALSE ] && CURSES_PARM="-u -0"
 
@@ -14,7 +16,7 @@ mkdir -p $LOGDIR
 usage() {
     echo "usage: [env=val *] $0" 1>&2
     echo " current env variables and their values: " 1>&2
-    for VAR in COOKIE DICT NGAMES NPLAYERS USE_GTK; do
+    for VAR in COOKIE DICT NGAMES NPLAYERS HOST PORT USE_GTK; do
         echo "$VAR:" $(eval "echo \$${VAR}") 1>&2
     done
     exit 0
@@ -35,7 +37,7 @@ for II in $(seq $NGAMES); do
     done
     for JJ in $(seq $NPLAYERS); do
         ./obj_linux_memdbg/xwords $CURSES_PARM -d $DICT -r Eric $REMOTES \
-            -C $COOKIE -q 2 2>${LOGDIR}/log_${II}_${JJ}.txt >/dev/null &
+            -a $HOST -p $PORT -C $COOKIE -q 2 2>${LOGDIR}/log_${II}_${JJ}.txt >/dev/null &
     done
 done
 
