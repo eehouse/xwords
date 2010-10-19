4a5c0832d483a5175044a368628753f3db6929ab
Andy2 <andy2@trand.private.network>
2010-10-19 07:54:10 -0700

    use new generalized code to check for messages.

--- xwords4/android/XWords4/src/org/eehouse/android/xw4/RelayReceiver.java
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/RelayReceiver.java
@@ -63,80 +63,9 @@ public class RelayReceiver extends BroadcastReceiver {
 
     private void query_relay( Context context ) 
     {
-        int[] nBytes = new int[1];
-        String[] ids = collectIDs( context, nBytes );
-        if ( null != ids && 0 < ids.length ) {
-            try {
-                Socket socket = 
-                    NetUtils.MakeProxySocket( context, 8000 );
-                DataOutputStream outStream = 
-                    new DataOutputStream( socket.getOutputStream() );
-
-                // total packet size
-                outStream.writeShort( 2 + nBytes[0] + ids.length + 1 );
-                Utils.logf( "total packet size: %d",
-                            2 + nBytes[0] + ids.length );
-
-                outStream.writeByte( NetUtils.PROTOCOL_VERSION );
-                outStream.writeByte( NetUtils.PRX_HAS_MSGS );
-
-                // number of ids
-                outStream.writeShort( ids.length );
-                Utils.logf( "wrote count %d to proxy socket",
-                            ids.length );
-
-                for ( String id : ids ) {
-                    outStream.writeBytes( id );
-                    outStream.write( '\n' );
-                }
-                outStream.flush();
-
-                DataInputStream dis = 
-                    new DataInputStream(socket.getInputStream());
-                Utils.logf( "reading from proxy socket" );
-                short result = dis.readShort();
-                short nameCount = dis.readShort();
-                short[] msgCounts = null;
-                if ( nameCount == ids.length ) {
-                    msgCounts = new short[nameCount];
-                    for ( int ii = 0; ii < nameCount; ++ii ) {
-                        msgCounts[ii] = dis.readShort();
-                        Utils.logf( "msgCounts[%d]=%d", ii, 
-                                    msgCounts[ii] );
-                    }
-                }
-                socket.close();
-                Utils.logf( "closed proxy socket" );
-
-                if ( null == msgCounts ) {
-                    Utils.logf( "relay has no messages" );
-                } else {
-                    ArrayList<String> idsWMsgs =
-                        new ArrayList<String>( nameCount );
-                    for ( int ii = 0; ii < nameCount; ++ii ) {
-                        if ( msgCounts[ii] > 0 ) {
-                            String msg = 
-                                String.format("%d messages for %s",
-                                              msgCounts[ii], 
-                                              ids[ii] );
-                            Utils.logf( msg );
-                            DBUtils.setHasMsgs( ids[ii] );
-                            idsWMsgs.add( ids[ii] );
-                        }
-                    }
-                    if ( 0 < idsWMsgs.size() ) {
-                        ids = new String[idsWMsgs.size()];
-                        setupNotification( context, idsWMsgs.toArray( ids ) );
-                    }
-                }
-
-            } catch( java.net.UnknownHostException uhe ) {
-                Utils.logf( uhe.toString() );
-            } catch( java.io.IOException ioe ) {
-                Utils.logf( ioe.toString() );
-            } catch( NullPointerException npe ) {
-                Utils.logf( npe.toString() );
-            }
+        String[] relayIDs = NetUtils.QueryRelay( context );
+        if ( null != relayIDs ) {
+            setupNotification( context, relayIDs );
         }
     }
 
@@ -163,19 +92,6 @@ public class RelayReceiver extends BroadcastReceiver {
         NotificationManager nm = (NotificationManager)
             context.getSystemService( Context.NOTIFICATION_SERVICE );
         nm.notify( R.string.relayids_extra, notification );
-    }
-
-    private String[] collectIDs( Context context, int[] nBytes )
-    {
-        String[] ids = DBUtils.getRelayIDNoMsgs( context );
-
-        int len = 0;
-        for ( String id : ids ) {
-            Utils.logf( "got relayID: %s", id );
-            len += id.length();
-        }
-        nBytes[0] = len;
-        return ids;
     }
 
     public static void RestartTimer( Context context )
