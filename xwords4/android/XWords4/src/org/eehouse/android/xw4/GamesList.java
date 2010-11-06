ceec9eda2854489d23093cd90be2d20bb2365117
Andy2 <andy2@trand.private.network>
2010-11-06 15:01:06 -0700

    set m_invalPath so new network game will be drawn correctly first time back in GamesList

--- xwords4/android/XWords4/src/org/eehouse/android/xw4/GamesList.java
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/GamesList.java
@@ -89,6 +89,7 @@ public class GamesList extends XWListActivity
                     String path = addGame( true );
                     GameUtils.doConfig( GamesList.this, path, 
                                         RelayGameActivity.class );
+                    m_invalPath = path;
                 }
             });
 
