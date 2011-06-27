c84722fc22c341df0d5c0511fcf67787399828b9
Andy2 <andy2@trand>
2011-06-27 06:45:14 -0700

    add ack

--- xwords4/relay/scripts/showrecent.sh
+++ xwords4/relay/scripts/showrecent.sh
@@ -21,7 +21,7 @@ while [ $# -gt 0 ]; do
 done
 
 QUERY="WHERE NOT -NTOTAL = sum_array(nperdevice)"
-COLUMNS="   dead,     connname,cid,room,pub,     lang,     ntotal,     nperdevice,        nsent,ctime,mtimes"
-COLUMNS_AS="dead as D,connname,cid,room,pub as P,lang as L,ntotal as n,nperdevice as nper,nsent,ctime,mtimes"
+COLUMNS="   dead,     connname,cid,room,pub,     lang,     ntotal,     nperdevice,        ack,nsent,ctime,mtimes"
+COLUMNS_AS="dead as D,connname,cid,room,pub as P,lang as L,ntotal as n,nperdevice as nper,ack,nsent,ctime,mtimes"
 
 echo "SELECT $COLUMNS_AS FROM ( SELECT $COLUMNS, unnest(mtimes) FROM games $QUERY) AS set GROUP BY $COLUMNS ORDER BY max(unnest) DESC LIMIT $LIMIT;" | psql xwgames
