999d4c11e74cf8db67e463460152f6a89186f4a0
Andy2 <andy2@trand>
2011-07-04 12:51:27 -0700

    simple script for watching what messages the relay has pending for a game.

---
+++ xwords4/relay/scripts/showmsgs.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e -u
+
+CONNNAME=""
+
+usage() {
+    echo "usage: $0 [--connname <s>]"
+}
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        --connname)
+            CONNNAME=$2
+            shift
+            ;;
+        *) usage
+            ;;
+    esac
+    shift
+done
+
+
+QUERY="SELECT * from msgs"
+if [ -n "$CONNNAME" ]; then
+    QUERY="$QUERY WHERE CONNNAME='$CONNNAME'"
+fi
+QUERY="$QUERY ORDER BY ctime DESC"
+
+echo "${QUERY};" | psql xwgames
+
