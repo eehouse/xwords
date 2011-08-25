79acccc6bfc7542d490c16b1b03969b828ce7c68
Andy2 <andy2@trand>
2011-08-25 06:52:12 -0700

    record, commented out, attempt to send NBS message.  sendDataMessage() crashes internally, and googling finds lots of reports/questions and no answers.  It appears NBS is broken on Android, at least for CDMA. Might want to revisit on a newer OS version that the 2.1 I'm running now.

---
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/NBSReceiver.java
@@ -0,0 +1,54 @@
+// /* -*- compile-command: "cd ../../../../../; ant install"; -*- */
+// /*
+//  * Copyright 2010 by Eric House (xwords@eehouse.org).  All rights
+//  * reserved.
+//  *
+//  * This program is free software; you can redistribute it and/or
+//  * modify it under the terms of the GNU General Public License as
+//  * published by the Free Software Foundation; either version 2 of the
+//  * License, or (at your option) any later version.
+//  *
+//  * This program is distributed in the hope that it will be useful, but
+//  * WITHOUT ANY WARRANTY; without even the implied warranty of
+//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  * General Public License for more details.
+//  *
+//  * You should have received a copy of the GNU General Public License
+//  * along with this program; if not, write to the Free Software
+//  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//  */
+
+// package org.eehouse.android.xw4;
+
+// import android.content.BroadcastReceiver;
+// import android.content.Context;
+// import android.content.Intent;
+
+// import android.telephony.SmsManager;
+
+// public class NBSReceiver extends BroadcastReceiver {
+
+//     @Override
+//     public void onReceive( Context context, Intent intent )
+//     {
+//         Utils.logf( "NBSReceiver::onReceive(intent=%s)!!!!",
+//                     intent.toString() );
+//     }
+
+//     static void tryNBSMessage()
+//     {
+//         byte[] data = { 'a', 'b', 'c' };
+
+//         SmsManager mgr = SmsManager.getDefault();
+
+//         try {
+//             mgr.sendDataMessage( "123-456-7890", null, (short)50009, 
+//                                  data, null, null );
+//             // PendingIntent sentIntent, 
+//             // PendingIntent deliveryIntent );
+//             Utils.logf( "sendDataMessage finished" );
+//         } catch ( IllegalArgumentException iae ) {
+//             Utils.logf( "%s", iae.toString() );
+//         }
+//     }
+// }
