b9a7574925fdacc74fb842d8e8da815454797e6a
Andy2 <andy2@trand>
2011-11-10 06:39:36 -0800

    fix emacs header comments; no code change

--- xwords4/common/mscore.c
+++ xwords4/common/mscore.c
@@ -1,6 +1,7 @@
-/* -*-mode: C; fill-column: 78; c-basic-offset: 4; -*- */
+/* -*- compile-command: "cd ../linux && make -j3 MEMDEBUG=TRUE"; -*- */
 /* 
- * Copyright 1998-2001 by Eric House (xwords@eehouse.org).  All rights reserved.
+ * Copyright 1998 - 2011 by Eric House (xwords@eehouse.org).  All rights
+ * reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
