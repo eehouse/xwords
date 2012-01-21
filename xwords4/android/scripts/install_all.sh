b4f95535791c17f69d85c759e22da0743429bfc3
Eric House <eehouse@trand>
2012-01-21 08:50:12 -0800

    install into devices too

--- xwords4/android/scripts/install_all.sh
+++ xwords4/android/scripts/install_all.sh
@@ -1,8 +1,46 @@
 #!/bin/sh
 
-for DEVICE in $(adb devices | grep '^emulator' | sed 's/device//'); do
-    adb -s $DEVICE install -r ./bin/XWords4-debug.apk
+set -u -e
+
+APK=./bin/XWords4-debug.apk
+
+usage() {
+    [ $# -ge 1 ] && echo "Error: $1"
+    echo "usage: $(basename $0) [-e] [-d]"
+    exit 1
+}
+
+[ -e $APK ] || usage "$APK not found"
+
+DEVICES=""
+
+while [ $# -ge 1 ]; do
+    case $1 in
+        -e)
+            DEVICES="$DEVICES $(adb devices | adb devices | grep '^emulator' | awk '{print $1}')"
+            ;;
+        -d)
+            DEVICES="$DEVICES $(adb devices | grep -v emulator | grep 'device$' | awk '{print $1}')"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+    shift
+done
+
+if [ -n "$DEVICES" ]; then
+    echo "installing this binary.  Check the age..."
+    ls -l $APK
+    echo ""
+fi
+
+COUNT=0
+
+for DEVICE in $DEVICES; do
+    echo $DEVICE
+    adb -s $DEVICE install -r $APK
     COUNT=$((COUNT+1))
 done
 
-echo "installed into $COUNT emulator instances"
+echo "installed into $COUNT devices/emulators"
