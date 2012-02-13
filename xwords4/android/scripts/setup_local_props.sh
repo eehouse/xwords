0f8ddadd69df2d28d0cf60ea6e21d9854d24514c
Eric House <eehouse@eehouse.org>
2012-02-12 21:23:50 -0800

    make work from more than one directory

--- xwords4/android/scripts/setup_local_props.sh
+++ xwords4/android/scripts/setup_local_props.sh
@@ -2,9 +2,6 @@
 
 echo $0
 
-cd $(dirname $0)
-cd ../XWords4
-
 # create local.properties for 1.6 sdk (target id 4).  Use 'android
 # list targets' to get the full set.
 android update project --path . --target android-7
