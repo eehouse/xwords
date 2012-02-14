cf9ab45e6fb71344e6a682786fc4a7aa53366852
Eric House <eehouse@trand>
2012-02-13 22:25:27 -0800

    finish first pass at build variant generation.  This works well enough that the app so generated runs in emulator, talks to relay, and does not crash.  Makefile has rules for .java, .xml and .h that run through sed to xw4 in import statements, package names etc so the variant app is in a different namespace.  Also honors any files in the variant tree that are actually checked into git should it be necessary to make changes beyond what sed can manage.

--- xwords4/android/scripts/Variant.mk
+++ xwords4/android/scripts/Variant.mk
@@ -1,1 +1,23 @@
 # -*- mode: Makefile; -*-
+
+$(DEST_PATH)/%.java : $(SRC_PATH)/%.java
+	@sed \
+		-e "s,\(package org.eehouse.android.\)xw4\(.*\);,\1$(VARIANT)\2;," \
+		-e "s,\(import org.eehouse.android.\)xw4\(.*\);,\1$(VARIANT)\2;," \
+		< $< > $@
+
+$(DEST_PATH)/%.png : $(SRC_PATH)/%.png
+	@cp $< $@
+
+$(DEST_PATH)/%.xml : $(SRC_PATH)/%.xml
+	@sed \
+		-e "s,\(^.*org.eehouse.android.\)xw4\(.*$$\),\1$(VARIANT)\2," \
+		< $< > $@
+
+$(DEST_PATH)/%.h : $(SRC_PATH)/%.h
+	sed \
+		-e "s,\(^.*org/eehouse/android/\)xw4\(.*$$\),\1$(VARIANT)\2," \
+		< $< > $@
+
+$(DEST_PATH)/% : $(SRC_PATH)/%
+	@cp $< $@
