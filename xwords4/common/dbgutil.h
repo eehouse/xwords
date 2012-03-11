5c11f1b284580a1711b230378f23b5497d37d0c8
Eric House <eehouse@trand>
2012-03-11 14:46:03 -0700

    add XP_LOGSTREAM

--- xwords4/common/dbgutil.h
+++ xwords4/common/dbgutil.h
@@ -26,4 +26,12 @@ const char* XP_Key_2str( XP_Key key );
 const char* DrawFocusState_2str( DrawFocusState dfs );
 const char* BoardObjectType_2str( BoardObjectType dfs );
 
+# ifdef DEBUG
+void dbg_logstream( XWStreamCtxt* stream, const char* func, int line );
+# define XP_LOGSTREAM( s )                      \
+    dbg_logstream( s, __func__, __LINE__ )
+# else
+# define XP_LOGSTREAM( s )
+# endif
+
 #endif
