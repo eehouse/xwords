b4242fe1233d829d8f920a0509c1b74db59fa6ff
Eric House <eehouse@trand>
2012-03-24 11:29:19 -0700

    use SDK-8 for now, and modify build scripts to set up properties based on SDK listed in AndroidManifest.xml.

--- xwords4/android/scripts/setup_local_props.sh
+++ xwords4/android/scripts/setup_local_props.sh
@@ -1,10 +1,32 @@
 #!/bin/sh
 
-echo $0
+set -u -e
+
+TARGET="android-7"
+
+usage() {
+    echo "usage: $0 [--target TARGET]"
+    exit 1
+}
+
+while [ $# -ge 1 ]; do
+    echo $1
+    case $1 in
+        --target)
+            shift
+            TARGET=$1
+            ;;
+        *)
+            echo "default: got $1"
+            usage
+            ;;
+    esac
+    shift
+done
 
 # create local.properties for 1.6 sdk (target id 4).  Use 'android
 # list targets' to get the full set.
-android update project --path . --target android-7
+android update project --path . --target $TARGET
 
 echo "local.properties looks like this:"
 echo ""
