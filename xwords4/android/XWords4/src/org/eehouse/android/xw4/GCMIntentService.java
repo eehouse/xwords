41cfacfee4a7499a0067837d9f96c9a6f86a8efc
Eric House <xwords@eehouse.org>
2012-11-05 06:20:55 -0800

    don't crash on emulator (which doesn't have library required by GCM)

--- xwords4/android/XWords4/src/org/eehouse/android/xw4/GCMIntentService.java
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/GCMIntentService.java
@@ -60,20 +60,24 @@ public class GCMIntentService extends GCMBaseIntentService {
         DbgUtils.logf( "GCMIntentService.init()" );
         int sdkVersion = Integer.valueOf(android.os.Build.VERSION.SDK);
         if ( 8 <= sdkVersion ) {
-            GCMRegistrar.checkDevice( app );
-            GCMRegistrar.checkManifest( app );
-            final String regId = GCMRegistrar.getRegistrationId( app );
-            if (regId.equals("")) {
-                DbgUtils.logf( "registering..." );
-                GCMRegistrar.register( app, GCMConsts.SENDER_ID );
-            } else {
-                DbgUtils.logf( "Already registered: id=\"%s\"", regId );
-            }
+            try {
+                GCMRegistrar.checkDevice( app );
+                GCMRegistrar.checkManifest( app );
+                final String regId = GCMRegistrar.getRegistrationId( app );
+                if (regId.equals("")) {
+                    DbgUtils.logf( "registering..." );
+                    GCMRegistrar.register( app, GCMConsts.SENDER_ID );
+                } else {
+                    DbgUtils.logf( "Already registered: id=\"%s\"", regId );
+                }
 
-            String curID = XWPrefs.getGCMDevID( app );
-            if ( ! curID.equals( regId ) ) {
-                DbgUtils.logf( "saved bad id: %s", curID );
-                XWPrefs.setGCMDevID( app, regId );
+                String curID = XWPrefs.getGCMDevID( app );
+                if ( ! curID.equals( regId ) ) {
+                    DbgUtils.logf( "saved bad id: %s", curID );
+                    XWPrefs.setGCMDevID( app, regId );
+                }
+            } catch ( UnsupportedOperationException uoe ) {
+                DbgUtils.showf( app, "Device can't do GCM; am I on an emulator?" );
             }
         }
     }
