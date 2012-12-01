38e6eac3a194035167d439f3fc02b0a636a8e324
Eric House <xwords@eehouse.org>
2012-12-01 10:10:28 -0800

    improve page users see when opening invite from new device on an old one.

--- xwords4/android/scripts/and_index.php
+++ xwords4/android/scripts/and_index.php
@@ -6,16 +6,16 @@ $g_apk = 'XWords4-release_android_beta_55-39-gbffb231.apk';
 function printHead() {
     print <<<EOF
 
-        <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-        <html>
-        <head>
-        <link rel="stylesheet" type="text/css" href="/xw4mobile.css" />
-        <title>Crosswords Invite redirect</title>
-        </head>
-        <body>
-          <p><em>UNDER CONSTRUCTION -- certain assumptions will go away before the next version ships</em></p>
-
-          <img src="../icon48x48.png"/>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="/xw4mobile.css" />
+    <title>Crosswords Invite redirect</title>
+  </head>
+  <body>
+    <div class="center">
+      <img class="center" src="../icon48x48.png"/>
+    </div>
 EOF;
 }
 
@@ -26,47 +26,59 @@ print <<<EOF
 EOF;
 }
 
-function printNonAndroid() {
+function printNonAndroid($agent) {
     $subject = "Android device not identified"; 
 
     $body = htmlentities("My browser is running on an android device but"
                          . " says its user agent is: \"$agent\".  Please fix your script to recognize"
                          . " this as an Android browser.");
     print <<<EOF
-        <p>Please open the link that sent you here on an Android device.</p>
-
-        <p>(If you <em>are</em> viewing this on an Android device, you&apos;ve
-            found a bug!  Please <a href="mailto:
-            xwords@eehouse.org?subject=$subject&body=$body">email me</a> (and be
-            sure to leave the user agent string in the email body.) 
-       </p>
+<div class="center">
+  <p>This page is meant to be viewed on an Android device.</p>
+  <hr>
+    <p>(If you <em>are</em> viewing this on an Android device, you&apos;ve
+      found a bug!  Please <a href="mailto:
+                                    xwords@eehouse.org?subject=$subject&body=$body">email me</a> (and be
+      sure to leave the user agent string in the email body.) 
+    </p>
+</div>
 
 EOF;
 }
 
 function printAndroid() {
 print <<<EOF
-<p>Hello Kati, Chris, Mana, Deb, and maybe Brynn,</p>
-
-<p>You&apos;ll have come here after clicking a link in an invite
-  email.  But you should not be seeing this page.</p>
+<div>
+<p>You&apos;ll have come here after clicking a link in an email or
+  text inviting you to a Crosswords game. But you should not be seeing
+  this page.</p>
 
 <p>If you got this page on your device, it means either
   <ul>
-    <li> you haven&apos;t installed a copy of Crosswords built after Nov. 25</li>
+    <li>The copy of Crosswords you have is NOT beta 56 or newer (dating from about Dec. 1, 2012).</li>
     <li> OR </li>
-    <li> that you have, and when you clicked on the link and were asked to
-      choose between a browser and Crosswords you chose the browser.</li>
+    <li> that your copy of Crosswords is new enough <em>BUT</em> that
+      when you clicked on the link and were asked to choose between a
+      browser and Crosswords you chose the browser.</li>
 </ul></p>
 
-<p>In the first
-case, <a href="http://eehouse.org/xw4/android/$g_apk">download
-and install the latest Crosswords</a>. Then go back to the invite
-email (or text) and tap the link again.</p>
+<p>In the first case, install the latest Crosswords,
+either <a href="market://search?q=pname:org.eehouse.android.xw4">via
+the Google Play store</a> or
+(sideloading) <a href="https://sourceforge.net/projects/xwords/files/xwords_Android/4.4%20beta%2056/XWords4-release_android_beta_56.apk/download">via
+Sourceforge.net</a>. After the install is finished go back to the
+invite email (or text) and tap the link again.</p>
 
-<p>In the second, hit your back button, click the link in your invite
-email (or text) again, and this time choose Crosswords.</p>
+<p>In the second case, hit your browser&apos;s back button, click the
+link in your invite email (or text) again, and this time let
+Crosswords handle it.</p>
 
+<p>Have fun.  And as always, <a href="mailto:xwords@eehouse.org">let
+me know</a> if you have problems or suggestions.</p>
+</div>
+<div class="center">
+  <img class="center" src="../icon48x48.png"/>
+</div>
 EOF;
 }
 
@@ -86,7 +98,7 @@ printHead();
 if ( /*true || */ $onFire || $onAndroid ) {
     printAndroid();
 } else {
-    printNonAndroid();
+    printNonAndroid($agent);
 }
 printTail();
 
