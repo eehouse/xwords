9f6baf39c49d308f4bc282feb1668dd69e9f8645
Eric House <xwords@eehouse.org>
2013-11-08 06:31:20 -0800

    don't insert rowid in any prefs list except the right one

--- xwords4/android/XWords4/src/org/eehouse/android/xw4/XWSumListPreference.java
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/XWSumListPreference.java
@@ -20,30 +20,36 @@
 
 package org.eehouse.android.xw4;
 
-import android.preference.ListPreference;
 import android.content.Context;
 import android.util.AttributeSet;
 
-import junit.framework.Assert;
+public class XWSumListPreference extends XWListPreference {
 
-public class XWListPreference extends ListPreference {
-    protected Context m_context;
-
-    public XWListPreference( Context context, AttributeSet attrs )
+    public XWSumListPreference( Context context, AttributeSet attrs )
     {
         super( context, attrs );
-        m_context = context;
     }
 
+    // Why I exist: insert the rowid line if debug is on
     protected void onAttachedToActivity()
     {
         super.onAttachedToActivity();
-        setSummary( getPersistedString( "" ) );
+
+        if ( XWPrefs.getDebugEnabled( m_context ) ) {
+            CharSequence[] entries = getEntries();
+            String lastRow
+                = m_context.getString( R.string.game_summary_field_rowid );
+            if ( !entries[entries.length - 1].equals( lastRow ) ) {
+                CharSequence[] newEntries = 
+                    new CharSequence[1 + entries.length];
+                System.arraycopy( entries, 0, newEntries, 0, 
+                                  entries.length );
+                newEntries[entries.length] = lastRow;
+                setEntries( newEntries );
+
+                setEntryValues( newEntries );
+            }
+        }
     }
 
-    protected boolean persistString( String value )
-    {
-        setSummary( value );
-        return super.persistString( value );
-    }
 }
