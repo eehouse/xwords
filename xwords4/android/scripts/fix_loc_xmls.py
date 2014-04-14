a0fd457ad1407a552ef7d7e13974474c19d78f0c
Eric House <xwords@eehouse.org>
2014-04-14 07:40:24 -0700

    add stuff, including ability to list strings missing in english or translated version

--- xwords4/android/scripts/fix_loc_xmls.py
+++ xwords4/android/scripts/fix_loc_xmls.py
@@ -15,9 +15,7 @@ def longestCommon( name, pairs ):
                 break
     return match
 
-def checkAgainst( path, pairs ):
-    print "looking at", path
-    doc = etree.parse( path )
+def checkAgainst( doc, pairs ):
     root = doc.getroot();
     done = False
     for child in root.iter():
@@ -52,27 +50,108 @@ def checkAgainst( path, pairs ):
                 # response = raw_input( "unknown name: %s; respond:" % (name) )
                 # print "you wrote:", response
 
-    # Now walk the doc, comparing names with the set in pairs and
-    # enforcing rules about names, offering to change whereever
-    # possible
-    out = open( path, "w" )
-    out.write( etree.tostring( doc, pretty_print=True, encoding="utf-8", xml_declaration=True ) )
+def findWithName( doc, name ):
+    result = None
+    for string in doc.findall('string'):
+        if string.get('name') == name:
+            result = string
+            break
+    print 'findWithName=>', result, 'for', name
+    return result
 
+def insertAfter( locRoot, englishElem, lastMatch, prevComments ):
+    name = englishElem.get('name')
+    text = englishElem.text
+    print "insertAfter(", locRoot, englishElem.get('name'), lastMatch.get('name'), prevComments, ")"
+    index = locRoot.getchildren().index(lastMatch)
+    print 'index:', index
+
+    for comment in prevComments:
+        commentNode = etree.Comment(comment)
+        index += 1
+        locRoot.insert( index, commentNode )
+
+    newNode = etree.fromstring('<string name="%s">XLATE ME: %s</string>' % (name, text))
+    index += 1
+    locRoot.insert( index, newNode )
+
+# For each name in pairs, check if it's in doc. If not, find the last
+# elem before it that is in doc and insert it after.  Start over each
+# time to avoid problems with iteration and order
+def doAddMissing( doc ):
+    done = False
+    while not done:
+        locRoot = doc.getroot()
+        lastMatch = None
+        prevComments = []
+        for elem in etree.parse("res/values/strings.xml").getroot().iter():
+            if not isinstance( elem.tag, basestring ):
+                prevComments.append( elem.text )
+                print "added comment:", elem.text
+            elif 'string' == elem.tag:
+                name = elem.get('name')
+                match = findWithName( locRoot, name )
+                print 'elem', name, 'has comments', prevComments
+                if None == match:
+                    print 'NO match for', name
+                    insertAfter( locRoot, elem, lastMatch, prevComments )
+                    done = True
+                    # sys.exit(0)
+                else:
+                    print 'got match for', name
+                    lastMatch = match
+                    lastComments = prevComments
+                prevComments = []
+                    
+def compare( engPairs, docPath ):
+    locStrings = mk_xml.getStrings( docPath )
+    engOnly = []
+    engOnly = [key for key in engPairs.keys() if not key in locStrings]
+    print "%d strings missing from %s: %s" % (len(engOnly), docPath, ", ".join(engOnly))
+    otherOnly = [key for key in locStrings.keys() if not key in engPairs]
+    print "%d strings missing from English: %s" % (len(otherOnly), ", ".join(otherOnly))
+
+def usage():
+    print "usage:", sys.argv[0]
+    print "   -a   # insert missing string elements for translation"
+    print "   -c   # compare each file with the English, listing string not in both"
+    print "   -f   # work on this strings.xml file (does all if none specified)"
+    print "   -x   # save any changes made (does not by default)"
+    sys.exit(1)
 
 def main():
     stringsFiles = []
-    pairs, rest = getopt.getopt(sys.argv[1:], "f:")
-    for option, value in pairs:
-        if option == '-f': stringsFiles.append(value)
+    addMissing = False
+    doSave = False
+    doCompare = False
+    try:
+        pairs, rest = getopt.getopt(sys.argv[1:], "acf:s")
+        for option, value in pairs:
+            if option == '-a': addMissing = True
+            if option == '-c': doCompare = True
+            elif option == '-f': stringsFiles.append(value)
+            elif option == '-s': doSave = True
+            else: usage()
+    except:
+        usage()
 
-    pairs = mk_xml.getStrings()
+    pairs = mk_xml.getStrings('res/values/strings.xml')
 
+    # Build list of files to work on
     if 0 == len(stringsFiles):
         for subdir, dirs, files in os.walk('res_src'):
             for file in [file for file in files if file == "strings.xml"]:
                 stringsFiles.append( "%s/%s" % (subdir, file) )
+
+    parser = etree.XMLParser(remove_blank_text=True)
     for path in stringsFiles:
-        checkAgainst( path, pairs )
+        doc = etree.parse(path, parser)
+        # checkAgainst( doc, pairs )
+        if addMissing: doAddMissing( doc )
+        if doCompare: compare( pairs, path )
+        if doSave:
+            out = open( path, "w" )
+            out.write( etree.tostring( doc, pretty_print=True, encoding="utf-8", xml_declaration=True ) )
 
 
 ##############################################################################
