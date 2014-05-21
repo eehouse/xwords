2293e6a730f5ec435fdd6b0fd5a4bce2ac519e0d
Eric House <xwords@eehouse.org>
2014-05-21 06:56:51 -0700

    remove unused imports

--- xwords4/android/XWords4/src/org/eehouse/android/xw4/BoardActivity.java
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/BoardActivity.java
@@ -26,9 +26,6 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 public class BoardActivity extends XWActivity {
 
