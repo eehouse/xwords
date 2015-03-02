0233ad6b79e5ada5680d194b038581de96d0d5e9
Eric House <xwords@eehouse.org>
2015-03-02 06:41:11 -0800

    turn off rematch

--- xwords4/android/XWords4/src/org/eehouse/android/xw4/XWApp.java
+++ xwords4/android/XWords4/src/org/eehouse/android/xw4/XWApp.java
@@ -36,7 +36,7 @@ public class XWApp extends Application {
     public static final boolean SMSSUPPORTED = true;
     public static final boolean GCMSUPPORTED = true;
     public static final boolean ATTACH_SUPPORTED = true;
-    public static final boolean REMATCH_SUPPORTED = true;
+    public static final boolean REMATCH_SUPPORTED = false;
     public static final boolean DEBUG_LOCKS = false;
     public static final boolean LOG_LIFECYLE = false;
     public static final boolean DEBUG_EXP_TIMERS = false;
