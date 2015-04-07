6dee7e37844d2391af8ebbf22736823c78b241a0
Eric House <xwords@eehouse.org>
2015-04-07 07:20:48 -0700

    remove warning about not using checked-in debug keystore from release builds that won't use it regardless

--- xwords4/android/scripts/key-setup.sh
+++ xwords4/android/scripts/key-setup.sh
@@ -2,16 +2,17 @@
 
 set -e -u
 
-HOMELOC=~/.android/debug.keystore
-HERELOC=$(pwd)/$(dirname $0)/debug.keystore
-# SCRIPTS=
+if [ $1 = 'debug' ]; then
 
-if [ -L $HOMELOC ]; then 
-	if cmp $(readlink $HOMELOC) $HERELOC; then
-		exit 0
-	fi
-elif [ -e $HOMELOC ]; then
-	cat << EOF
+	HOMELOC=~/.android/debug.keystore
+	HERELOC=$(pwd)/$(dirname $0)/debug.keystore
+
+	if [ -L $HOMELOC ]; then 
+		if cmp $(readlink $HOMELOC) $HERELOC; then
+			exit 0
+		fi
+	elif [ -e $HOMELOC ]; then
+		cat << EOF
 * You are using a different keystore from what's part of this project.
 * You won't be able to install this over something built from a
 * different machine.  If that's not ok, remove it (i.e. run this on a
@@ -19,7 +20,9 @@ elif [ -e $HOMELOC ]; then
 rm $HOMELOC
 * and rerun ant to use the built-in keystore.
 EOF
-else
-	echo "$0: creating link at $HOMELOC"
-	ln -sf  $HERELOC $HOMELOC
+	else
+		echo "$0: creating link at $HOMELOC"
+		ln -sf  $HERELOC $HOMELOC
+	fi
+
 fi
