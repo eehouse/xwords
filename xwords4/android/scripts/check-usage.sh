c0e8f9cb65d589ac0ef13bc82338943c02a35fae
Eric House <xwords@eehouse.org>
2016-01-23 21:06:37 -0800

    look for plurals too

--- xwords4/android/scripts/check-usage.sh
+++ xwords4/android/scripts/check-usage.sh
@@ -7,24 +7,32 @@ set -e -u
 
 cd $(dirname $0)
 
-IDS=$(./string-names.sh ../XWords4/res/values/strings.xml)
-STR_COUNT=$(echo $IDS | wc -w)
+checkStrings() {
+	TYP=$1
+	IDS="$2"
+	STR_COUNT=$(echo $IDS | wc -w)
 
-JAVA_FILES=$(find ../ -name '*.java')
-XML_FILES="$(find ../XWords4/res/ -name '*.xml') ../XWords4/AndroidManifest.xml"
-FILE_COUNT=$(( $(echo $JAVA_FILES | wc -w) + $(echo $XML_FILES | wc -w) ))
+	JAVA_FILES=$(find ../ -name '*.java')
+	XML_FILES="$(find ../XWords4/res/ -name '*.xml') ../XWords4/AndroidManifest.xml"
+	FILE_COUNT=$(( $(echo $JAVA_FILES | wc -w) + $(echo $XML_FILES | wc -w) ))
 
-echo "checking $STR_COUNT string ids in $FILE_COUNT files..."
+	echo "checking $STR_COUNT $TYP ids in $FILE_COUNT files..."
 
-for ID in $IDS; do
-    RID="R.string.${ID}"
-    if grep -qr $RID $JAVA_FILES; then
-        continue;
-    fi
+	for ID in $IDS; do
+		RID="R.${TYP}.${ID}"
+		if grep -qr $RID $JAVA_FILES; then
+			continue;
+		fi
 
-    RID="@string/${ID}"
-    if grep -qr $RID $XML_FILES; then
-        continue;
-    fi
-    echo "$ID not found"
-done
+		RID="@string/${ID}"
+		if grep -qr $RID $XML_FILES; then
+			continue;
+		fi
+		echo "$ID not found"
+	done
+}
+
+STR_IDS=$(./string-names.sh ../XWords4/res/values/strings.xml)
+checkStrings string "$STR_IDS"
+PLRL_IDS=$(./plurals-names.sh ../XWords4/res/values/strings.xml)
+checkStrings plurals "$PLRL_IDS"
