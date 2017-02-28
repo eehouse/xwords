d865812002195800fb2e04800a30c95cd464c788
Eric House <xwords@eehouse.org>
2017-02-27 18:31:17 -0800

    failing to find an .apk is not an error

--- xwords4/android/scripts/find-and-gradle.sh
+++ xwords4/android/scripts/find-and-gradle.sh
@@ -56,20 +56,25 @@ touch $NOW_FILE
 # If this fails, the "set -e" above means we won't try to install anything
 ./gradlew $*
 
-# Find the newest apk in the build output directory that's newer than our timestamp
+# Find the newest apk in the build output directory that's newer than
+# our timestamp. Don't fail if there isn't one -- we might be building
+# aars or something. gradlew failing will be our clue that something
+# went wrong up to this point.
+
 APK=''
-APKS=$(find app/build/outputs/apk/ -name '*.apk' -a -newer $NOW_FILE)
+APKS=$(find app/build/outputs/apk/ -name '*.apk' -a -newer $NOW_FILE 2> /dev/null) || true
 if [ -n "$APKS" ]; then
 	APK=$(ls -t $APKS | head -n 1)
 fi
-[ -n "$APK" ] || usage "no new .apk files found; did the build succeed?"
 
-if [ -n "$UNINSTALL" ]; then
-	LINE=$(aapt dump badging $APK | grep '^package')
-	PACKAGE=$(echo $LINE | sed "s,.*name='\([^' ]*\)'.*,\1,")
-	adb shell pm uninstall $PACKAGE
+if [ -n "$APK" ]; then
+	if [ -n "$UNINSTALL" ]; then
+		LINE=$(aapt dump badging $APK | grep '^package')
+		PACKAGE=$(echo $LINE | sed "s,.*name='\([^' ]*\)'.*,\1,")
+		adb shell pm uninstall $PACKAGE
+	fi
 fi
 
-if [ -n "$INSTALL" ]; then
-	adb-install.sh -p $APK
-fi
+# if [ -n "$INSTALL" ]; then
+# 	adb-install.sh -p $APK || true
+# fi
