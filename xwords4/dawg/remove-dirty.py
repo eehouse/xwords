8752432de3a7d2d5aeef1a16b570d08e3b12d70a
Eric House <xwords@eehouse.org>
2017-05-04 22:45:27 -0700

    add ability to filter out "dirty" words
    If a Makefile defines a dirty word list then a new python script is
    invoked to filter for and remove those words as the dict is being
    built. So far I have for English only, which makes sense because only
    English wordlists are built-in on Android and Google's rating system
    cares only about what's built in.

---
+++ xwords4/dawg/remove-dirty.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# Invoked with path to bad words list as single parameter, and with a
+# stream of words via stdin, loads the bad words into a map and for
+# every word in stdin echos it to stdout IFF it's not in the map.
+
+import sys
+
+dirtyMap = {}
+dirtyList = sys.argv[1]
+for f in open(dirtyList):
+    dirtyMap[f] = True
+
+for word in sys.stdin:
+    if word in dirtyMap:
+        sys.stderr.write( sys.argv[0] + ": dropping: " + word )
+    else:
+        sys.stdout.write( word )
