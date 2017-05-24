3c650ce762083f6db9e0756faf38373ecc1dde8f
Eric House <xwords@eehouse.org>
2017-05-23 18:38:11 -0700

    fix xw4d build (shortening more xw4dbg)

--- xwords4/android/scripts/Variant.mk
+++ xwords4/android/scripts/Variant.mk
@@ -1,6 +1,6 @@
 # -*- mode: Makefile; -*-
 
-src/xw4dbg/res/values/strings.xml: src/main/res/values/strings.xml
+src/xw4d/res/values/strings.xml: src/main/res/values/strings.xml
 	@mkdir -p $(shell dirname $@)
 	@sed \
 		-e "s,CrossWords,$(APPNAME),g" \
