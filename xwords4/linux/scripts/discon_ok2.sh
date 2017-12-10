2e71aedc024bf4ba504973afb0fbdd74e0ad1ab5
Eric House <xwords@eehouse.org>
2017-12-09 21:32:05 -0800

    fix --game-dict option

--- xwords4/linux/scripts/discon_ok2.sh
+++ xwords4/linux/scripts/discon_ok2.sh
@@ -43,7 +43,7 @@ declare -A LOGS
 declare -A MINEND
 declare -A ROOM_PIDS
 declare -a APPS_OLD=()
-declare -a DICTS=				# wants to be =() too?
+declare -a DICTS=()				# wants to be =() too?
 declare -A CHECKED_ROOMS
 
 function cleanup() {
