74406c9bba817db772c5c6300b3a8cc8622e9cfc
Eric House <xwords@eehouse.org>
2018-03-09 06:45:21 -0800

    on failure, don't log what looks like success

--- xwords4/relay/xwrelay.cpp
+++ xwords4/relay/xwrelay.cpp
@@ -691,12 +691,14 @@ send_msg_via_udp( const AddrInfo* addr, AddrInfo::ClientToken clientToken,
         uint32_t asNetTok = htonl(clientToken);
         ssize_t nSent = send_via_udp( addr, packetIDP, XWPDEV_MSG, &asNetTok, 
                                       sizeof(asNetTok), buf, bufLen, NULL );
-        logf( XW_LOGINFO, "%s: sent %d bytes (plus header) on UDP socket, "
-              "token=%x(%d)", __func__, bufLen, clientToken, 
-              clientToken );
         result = 0 < nSent;
-        logf( XW_LOGINFO, "%s()=>%d", __func__, result );
+        if (result) {
+            logf( XW_LOGINFO, "%s: sent %d bytes (plus header) on UDP socket, "
+                  "token=%x(%d)", __func__, bufLen, clientToken,
+                  clientToken );
+        }
     }
+    // logf( XW_LOGINFO, "%s()=>%d", __func__, result );
     return result;
 }
 
