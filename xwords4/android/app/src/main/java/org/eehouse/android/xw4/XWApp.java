fdf27e901a300770deab82fbfff05a7752c46b15
Eric House <xwords@eehouse.org>
2018-12-05 20:36:25 -0800

    don't try to connect to non-phones
    When a device is paired but not a phone (or computer, which is how
    tablets identify), don't bother trying to scan it.

--- xwords4/android/app/src/main/java/org/eehouse/android/xw4/XWApp.java
+++ xwords4/android/app/src/main/java/org/eehouse/android/xw4/XWApp.java
@@ -50,8 +50,6 @@ public class XWApp extends Application implements LifecycleObserver {
     public static final boolean LOCUTILS_ENABLED = false;
     public static final boolean CONTEXT_MENUS_ENABLED = true;
     public static final boolean OFFER_DUALPANE = false;
-    // BT class "COMPUTERS" includes tablets like the Nexus 9
-    public static final boolean BT_SCAN_COMPUTERS = true;
 
     public static final String SMS_PUBLIC_HEADER = "-XW4";
     public static final int MAX_TRAY_TILES = 7; // comtypes.h
