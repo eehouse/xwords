1e5024f4d5d084c064bc3f87aae0fb81ed4ecc09
Eric House <xwords@eehouse.org>
2019-05-20 06:26:57 -0700

    remove wince files

--- xwords4/wince/test/messagebox.c
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-
-int
-WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine,
-         int nCmdShow )
-{
-    MessageBox(0, L"HELLO!", L"H3LLO!", 0);
-    return 0;
-}
