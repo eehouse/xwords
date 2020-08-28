eehouse/xwords

commit 15f95b52a924781dc1f0ff307db58785c365f19f

hack to recover from stalled by-room-name games

xwords4/common/server.c
@@ -530,6 +530,14 @@ server_makeFromStream( MPFORMAL XWEnv xwe, XWStreamCtxt* stream, ModelCtxt* mode
         server->nv.prevWordsStream = readStreamIf( server, stream );
     }
 
+    /* Hack alert: recovering from an apparent bug that leaves the game
+       thinking it's a client but being in the host-only XWSTATE_BEGIN
+       state. */
+    if ( server->nv.gameState == XWSTATE_BEGIN && !amServer(server) ) {
+        XP_LOGFF( "server_makeFromStream(): fixing state" );
+        SETSTATE( server, XWSTATE_NONE );
+    }
+
     informMissing( server, xwe );
     return server;
 } /* server_makeFromStream */

commit 5998132d4b56ea48972d229f894d5491293b93bd

fix standalone hang introduced by 15f95b52a

xwords4/common/server.c
@@ -533,7 +533,8 @@ server_makeFromStream( MPFORMAL XWEnv xwe, XWStreamCtxt* stream, ModelCtxt* mode
     /* Hack alert: recovering from an apparent bug that leaves the game
        thinking it's a client but being in the host-only XWSTATE_BEGIN
        state. */
-    if ( server->nv.gameState == XWSTATE_BEGIN && !amServer(server) ) {
+    if ( server->nv.gameState == XWSTATE_BEGIN &&
+         server->vol.gi->serverRole == SERVER_ISCLIENT ) {
         XP_LOGFF( "server_makeFromStream(): fixing state" );
         SETSTATE( server, XWSTATE_NONE );
     }