d9ebf69ab59dfaca860b50828755f1a94532c598
Eric House <xwords@eehouse.org>
2021-03-08 10:35:07 -0800

    route logs to console.log, not .error

--- xwords4/wasm/wasmutls.c
+++ xwords4/wasm/wasmutls.c
@@ -1,4 +1,4 @@
-/* -*- compile-command: "cd ../wasm && make main.html -j3"; -*- */
+/* -*- compile-command: "cd ../wasm && make MEMDEBUG=TRUE install -j3"; -*- */
 /*
  * Copyright 2021 by Eric House (xwords@eehouse.org).  All rights reserved.
  *
@@ -17,20 +17,28 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#include <emscripten.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 
 #include "wasmutls.h"
 
+EM_JS(void, console_log, (const char* msg), {
+        let jsmsg = UTF8ToString(msg);
+        console.log(jsmsg);
+    });
+
 void 
 wasm_debugf( const char* format, ... )
 {
     va_list ap;
     va_start(ap, format);
-    vfprintf( stderr, format, ap );
+    char buf[1024];
+    vsnprintf(buf, sizeof(buf)-1, format, ap);
+    buf[sizeof(buf)-1] = '\0';  /* to be safe */
     va_end( ap );
-    fprintf( stderr, "\n" );
+    console_log(buf);
 }
 
 void
@@ -38,11 +46,15 @@ wasm_debugff( const char* func, const char* file, const char* fmt, ...)
 {
     char buf[1024];
     snprintf( buf, sizeof(buf), "%s:%s(): %s", file, func, fmt );
+    buf[sizeof(buf)-1] = '\0';  /* to be safe */
+
     va_list ap;
     va_start( ap, fmt );
-    vfprintf( stderr, buf, ap );
+    char buf2[1024];
+    vsnprintf( buf2, sizeof(buf2), buf, ap );
     va_end( ap );
-    fprintf( stderr, "\n" );
+    buf2[sizeof(buf2)-1] = '\0';  /* to be safe */
+    console_log( buf2 );
 }
 
 #ifndef MEM_DEBUG
