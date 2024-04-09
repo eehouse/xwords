182ce9b7f0e62f41faba6d39a3ec9fde371a3c91
Eric House <eehouse@eehouse.org>
2024-04-09 08:23:59 -0700

    add option to build-debug script

--- xwords4/android/scripts/build-debug.sh
+++ xwords4/android/scripts/build-debug.sh
@@ -3,6 +3,7 @@
 set -e -u
 
 NO_RM=''
+NO_UPLOAD=''
 
 DIR=/tmp/build_$$_dir
 mkdir -p $DIR
@@ -10,7 +11,7 @@ pushd $DIR
 
 usage() {
     [ $# -ge 1 ] && echo "Error: $1"
-	echo "usage: $0 [--no-rm]    # do not remove the build directory"
+	echo "usage: $0 [--no-rm --no-upload]    # do not remove the build directory"
     echo "builds debug variant from the current tip of github"
 	echo "(last modified Jan 2024)"
     exit 1
@@ -24,6 +25,9 @@ while [ $# -ge 1 ]; do
 		--no-rm)
 			NO_RM=1
 			;;
+		--no-upload)
+			NO_UPLOAD=1
+			;;
 		*)
 			usage "unexpected command $1"
 			;;
@@ -36,7 +40,9 @@ cd xwords/xwords4/android
 ./gradlew asXw4dDeb
 
 APK="$(find . -name '*.apk')"
-if [ -n "${XW4D_UPLOAD}" ]; then
+if [ -n "${NO_UPLOAD}" ]; then
+	: # do nothing
+elif [ -n "${XW4D_UPLOAD}" ]; then
 	IFS=","
 	for UPPATH in ${XW4D_UPLOAD}; do
 		scp "$APK" ${UPPATH}
