11859f08c5bad036dedc243f645fb00f70f42c35
Eric House <eehouse@eehouse.org>
2024-05-06 19:32:32 -0700

    two more files, AS plus me

---
+++ xwords4/android/app/src/main/java/org/eehouse/android/xw4/TilePickAlert.kt
@@ -0,0 +1,167 @@
+/* -*- compile-command: "find-and-gradle.sh inXw4dDeb"; -*- */
+/*
+ * Copyright 2017 by Eric House (xwords@eehouse.org).  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.eehouse.android.xw4
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import org.eehouse.android.xw4.DlgDelegate.DlgClickNotify
+import org.eehouse.android.xw4.TilePickView.TilePickListener
+import org.eehouse.android.xw4.loc.LocUtils
+import java.io.Serializable
+
+class TilePickAlert : XWDialogFragment(), TilePickListener {
+    private var mView: TilePickView? = null
+    private var mState: TilePickState? = null
+    private var mAction: DlgDelegate.Action? = null
+    private var mDialog: AlertDialog? = null
+    private var mSelTiles = IntArray(0)
+
+    class TilePickState : Serializable {
+        @JvmField
+        var col = 0
+        @JvmField
+        var row = 0
+        @JvmField
+        var playerNum: Int
+        @JvmField
+        var counts: IntArray? = null
+        @JvmField
+        var faces: Array<String>
+        @JvmField
+        var isInitial = false
+        @JvmField
+        var nToPick: Int
+
+        constructor(player: Int, faces: Array<String>, col: Int, row: Int) {
+            this.col = col
+            this.row = row
+            playerNum = player
+            this.faces = faces
+            nToPick = 1
+        }
+
+        constructor(
+            isInitial: Boolean, playerNum: Int, nToPick: Int,
+            faces: Array<String>, counts: IntArray?
+        ) {
+            this.playerNum = playerNum
+            this.isInitial = isInitial
+            this.nToPick = nToPick
+            this.faces = faces
+            this.counts = counts
+        }
+
+        fun forBlank(): Boolean {
+            return null == counts
+        }
+    }
+
+    override fun onSaveInstanceState(bundle: Bundle) {
+        bundle.putSerializable(TPS, mState)
+        bundle.putSerializable(ACTION, mAction)
+        mView!!.saveInstanceState(bundle)
+        super.onSaveInstanceState(bundle)
+    }
+
+    override fun onCreateDialog(sis: Bundle?): Dialog {
+        var sis = sis
+        if (null == sis) {
+            sis = arguments
+        }
+        mState = sis!!.getSerializable(TPS) as TilePickState?
+        mAction = sis.getSerializable(ACTION) as DlgDelegate.Action?
+        val activity: Activity? = activity
+        Assert.assertNotNull(activity)
+        mView = LocUtils.inflate(activity, R.layout.tile_picker) as TilePickView
+        mView!!.init(this, mState, sis)
+        val resId =
+            if (mState!!.forBlank()) R.string.title_blank_picker else R.string.tile_tray_picker
+        val ab = LocUtils.makeAlertBuilder(activity)
+            .setTitle(resId)
+            .setView(mView)
+        if (!mState!!.forBlank()) {
+            val lstnr = DialogInterface.OnClickListener { dialog, which -> onDone() }
+            ab.setPositiveButton(buttonTxt(), lstnr)
+        }
+        mDialog = ab.create()
+        return mDialog!!
+    }
+
+    override fun getFragTag(): String {
+        return TAG
+    }
+
+    // TilePickView.TilePickListener interface
+    override fun onTilesChanged(nToPick: Int, newTiles: IntArray) {
+        mSelTiles = newTiles
+        val haveAll = nToPick == newTiles.size
+        if (haveAll && mState!!.forBlank()) {
+            onDone()
+        } else if (null != mDialog) {
+            mDialog!!.getButton(AlertDialog.BUTTON_POSITIVE).text = buttonTxt()
+        }
+    }
+
+    override fun onCancel(dialog: DialogInterface) {
+        super.onCancel(dialog)
+        val activity: Activity? = activity
+        if (activity is DlgClickNotify) {
+            val notify = activity as DlgClickNotify
+            notify.onDismissed(mAction)
+        }
+    }
+
+    private fun onDone() {
+        val activity: Activity? = activity
+        if (activity is DlgClickNotify) {
+            val notify = activity as DlgClickNotify
+            notify.onPosButton(mAction, mState, mSelTiles)
+        } else {
+            Assert.failDbg()
+        }
+        dismiss()
+    }
+
+    private fun buttonTxt(): String {
+        val context = context
+        val left = mState!!.nToPick - mSelTiles.size
+        return if (0 == left) LocUtils.getString(
+            context,
+            android.R.string.ok
+        ) else LocUtils.getString(context, R.string.tilepick_all_fmt, left)
+    }
+
+    companion object {
+        private val TAG = TilePickAlert::class.java.getSimpleName()
+        private const val TPS = "TPS"
+        private const val ACTION = "ACTION"
+        @JvmStatic
+        fun newInstance(action: DlgDelegate.Action?, state: TilePickState?): TilePickAlert {
+            val result = TilePickAlert()
+            val args = Bundle()
+            args.putSerializable(ACTION, action)
+            args.putSerializable(TPS, state)
+            result.setArguments(args)
+            return result
+        }
+    }
+}
---
+++ xwords4/android/app/src/main/java/org/eehouse/android/xw4/TilePickView.kt
@@ -0,0 +1,175 @@
+/* -*- compile-command: "find-and-gradle.sh inXw4dDeb"; -*- */
+/*
+ * Copyright 2009-2024 by Eric House (xwords@eehouse.org).  All rights
+ * reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.eehouse.android.xw4
+
+import android.content.Context
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import org.eehouse.android.xw4.TilePickAlert.TilePickState
+import org.eehouse.android.xw4.loc.LocUtils
+
+class TilePickView(context: Context?, `as`: AttributeSet?) : LinearLayout(context, `as`) {
+    interface TilePickListener {
+        fun onTilesChanged(nToPick: Int, newTiles: IntArray)
+    }
+
+    private var mPendingTiles: ArrayList<Int>? = null
+    private var mListner: TilePickListener? = null
+    private var mState: TilePickState? = null
+    private val mButtons: MutableMap<Int, Button> = HashMap()
+    internal fun init(
+        lstn: TilePickListener?, state: TilePickState?,
+        bundle: Bundle
+    ) {
+        mState = state
+        mListner = lstn
+        mPendingTiles = bundle.getSerializable(NEW_TILES) as ArrayList<Int>?
+        if (null == mPendingTiles) {
+            Log.d(TAG, "creating new mPendingTiles")
+            mPendingTiles = ArrayList()
+        }
+        showPending()
+        addTileButtons()
+        updateDelButton()
+        findViewById<View>(R.id.del).setOnClickListener(object : OnClickListener {
+            override fun onClick(view: View) {
+                removePending()
+                updateDelButton()
+                mListner!!.onTilesChanged(mState!!.nToPick, pending)
+            }
+        })
+        mListner!!.onTilesChanged(mState!!.nToPick, pending)
+    }
+
+    // NOT @Override!!!
+    fun saveInstanceState(bundle: Bundle) {
+        bundle.putSerializable(NEW_TILES, mPendingTiles)
+    }
+
+    private val pending: IntArray
+        private get() {
+            val result = IntArray(mPendingTiles!!.size)
+            for (ii in result.indices) {
+                result[ii] = mPendingTiles!![ii]
+            }
+            return result
+        }
+
+    private fun addTileButtons() {
+        val context = context
+        val container = findViewById<View>(R.id.button_bar_container) as LinearLayout
+        var bar: LinearLayout? = null
+        var barLen = 0
+        var nShown = 0
+        for (ii in mState!!.faces.indices) {
+            if (null != mState!!.counts && mState!!.counts!![ii] == 0 && !SHOW_UNAVAIL) {
+                continue
+            }
+            val visIndex = nShown++
+            if (null == bar || 0 == visIndex % barLen) {
+                bar = LocUtils.inflate(context, R.layout.tile_picker_bar) as LinearLayout
+                container.addView(bar)
+                barLen = bar.childCount
+            }
+            val button = bar!!.getChildAt(visIndex % barLen) as Button
+            mButtons[ii] = button
+            button.visibility = VISIBLE
+            updateButton(ii, 0)
+            button.setOnClickListener { view -> onTileClicked(view, ii) }
+        }
+    }
+
+    private fun onTileClicked(view: View, index: Int) {
+        // replace the last pick if we don't have room to add a new one
+        if (mPendingTiles!!.size == mState!!.nToPick) {
+            removePending()
+        }
+        mPendingTiles!!.add(index)
+        updateDelButton()
+        updateButton(index, -1)
+        showPending()
+        mListner!!.onTilesChanged(mState!!.nToPick, pending)
+    }
+
+    private fun showPending() {
+        val desc = findViewById<View>(R.id.pending_desc) as TextView
+        if (mState!!.forBlank()) {
+            desc.visibility = GONE
+        } else {
+            val faces: MutableList<String?> = ArrayList()
+            for (indx in mPendingTiles!!) {
+                faces.add(mState!!.faces[indx])
+            }
+            desc.text = LocUtils.getString(
+                context,
+                R.string.tile_pick_summary_fmt,
+                TextUtils.join(",", faces)
+            )
+        }
+    }
+
+    private fun pendingCount(dataIndex: Int): Int {
+        var count = 0
+        for (index in mPendingTiles!!) {
+            if (index == dataIndex) {
+                ++count
+            }
+        }
+        return count
+    }
+
+    private fun updateButton(index: Int, adjust: Int) {
+        val button = mButtons[index]
+        val context = context
+        var face: String? = mState!!.faces[index]
+        if (!mState!!.forBlank()) {
+            val count = mState!!.counts!![index] - pendingCount(index)
+            face = LocUtils.getString(
+                context, R.string.tile_button_txt_fmt,
+                face, count
+            )
+            val vis = if (count == 0) INVISIBLE else VISIBLE
+            button!!.visibility = vis
+        }
+        button!!.text = face
+    }
+
+    private fun removePending() {
+        val tile = mPendingTiles!!.removeAt(mPendingTiles!!.size - 1)
+        updateButton(tile, 1)
+        showPending()
+    }
+
+    private fun updateDelButton() {
+        val vis = if (mState!!.forBlank() || mPendingTiles!!.size == 0) INVISIBLE else VISIBLE
+        findViewById<View>(R.id.del).visibility = vis
+    }
+
+    companion object {
+        private val TAG = TilePickView::class.java.getSimpleName()
+        private const val NEW_TILES = "NEW_TILES"
+        private const val SHOW_UNAVAIL = false
+    }
+}
