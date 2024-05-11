31f4eb538f18c47ca635e8f41001ebb6ab630539
Eric House <eehouse@eehouse.org>
2024-05-10 21:47:40 -0700

    AS got this too

--- xwords4/android/app/src/main/java/org/eehouse/android/xw4/InviteActivity.kt
+++ xwords4/android/app/src/main/java/org/eehouse/android/xw4/InviteActivity.kt
@@ -1,6 +1,6 @@
 /* -*- compile-command: "find-and-gradle.sh inXw4dDeb"; -*- */
 /*
- * Copyright 2009-2014 by Eric House (xwords@eehouse.org).  All
+ * Copyright 2009 - 2024 by Eric House (xwords@eehouse.org).  All
  * rights reserved.
  *
  * This program is free software; you can redistribute it and/or
@@ -17,19 +17,14 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+package org.eehouse.android.xw4
 
-package org.eehouse.android.xw4;
+import android.os.Bundle
+import android.view.Window
 
-import android.os.Bundle;
-import android.view.Window;
-
-public abstract class InviteActivity extends XWActivity {
-
-    @Override
-    protected void onCreate( Bundle savedInstanceState )
-    {
-        super.onCreate( savedInstanceState );
-        requestWindowFeature( Window.FEATURE_NO_TITLE );
+abstract class InviteActivity : XWActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
     }
-
 }
