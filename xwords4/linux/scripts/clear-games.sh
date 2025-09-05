1ac0c7cd9ba66e94e789bdbe6f7277f3d501e062
Eric House <eehouse@eehouse.org>
2025-09-05 14:21:34 -0700

    fix script to allow specifying db

--- xwords4/linux/scripts/clear-games.sh
+++ xwords4/linux/scripts/clear-games.sh
@@ -1,5 +1,38 @@
-#!/bin/sh
+#!/bin/bash
+set -e -u
 
-echo "delete from pairs where key like 'games/%';" | sqlite3 xwgames.sqldb
-echo "delete from pairs where key like 'groups/%';" | sqlite3 xwgames.sqldb
-echo "delete from pairs where key like 'gmgr/state';" | sqlite3 xwgames.sqldb
+DB=xwgames.sqldb
+
+usage() {
+    cat <<EOF
+usage: $0
+      [--db <path>]  # default: $DB
+Removes game and group data from a linux xwords db.
+EOF
+    exit 0
+}
+
+while [[ ! -z ${1+x} ]]; do
+    case $1 in
+		--db)
+			DB=$2
+			shift
+			;;
+		--help)
+			usage
+			;;
+        *)
+			echo "Unexpected parameter $1" >&2
+            usage
+            ;;
+	esac
+	shift
+done
+
+if [ -f $DB ]; then
+	echo "delete from pairs where key like 'games/%';" | sqlite3 $DB
+	echo "delete from pairs where key like 'groups/%';" | sqlite3 $DB
+	echo "delete from pairs where key like 'gmgr/state';" | sqlite3 $DB
+else
+	echo "file $DB does not exist"
+fi
