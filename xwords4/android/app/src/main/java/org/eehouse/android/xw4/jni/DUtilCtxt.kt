c8f5940f12af18465f1b649a06f973cd8d98c97a
Eric House <eehouse@eehouse.org>
2025-09-17 19:28:18 -0700

    start filling in BT send, at least to fix assertion failure
    BT's a work in progress. I may just switch to BTLE, forcing everybody
    in a game to be running the newest build.

--- xwords4/android/app/src/main/java/org/eehouse/android/xw4/jni/DUtilCtxt.kt
+++ xwords4/android/app/src/main/java/org/eehouse/android/xw4/jni/DUtilCtxt.kt
@@ -35,6 +35,7 @@ import org.json.JSONObject
 import java.lang.ref.WeakReference
 
 import org.eehouse.android.xw4.Assert
+import org.eehouse.android.xw4.BTUtils
 import org.eehouse.android.xw4.BoardDelegate
 import org.eehouse.android.xw4.BuildConfig
 import org.eehouse.android.xw4.Channels
@@ -360,6 +361,10 @@ class DUtilCtxt() {
 
     fun sendViaMQTT(topic: String, msg: ByteArray, qos: Int) {
         MQTTUtils.send(mContext, topic, msg, qos)
+    }
+
+    fun sendViaBT(msg: ByteArray, hostName: String, hostAddr: String?) {
+        BTUtils.sendMessage(mContext, msg, hostName, hostAddr)
     }
 
     fun sendViaNBS(msg: ByteArray, phone: String, port: Int) {
